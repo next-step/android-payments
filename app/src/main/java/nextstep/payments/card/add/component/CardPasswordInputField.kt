b9fc33f060a817894823382441ea55e00aeaacb2
malibinYun <nightmare73@naver.com>
2024-02-24 02:10:29 +0900

    카드 비밀번호 입력창 컴포넌트 추가

---
+++ app/src/main/java/nextstep/payments/card/add/component/CardPasswordInputField.kt
@@ -0,0 +1,29 @@
+package nextstep.payments.card.add.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import nextstep.payments.R
+
+@Composable
+fun CardPasswordInputField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+) {
+    InputField(
+        label = stringResource(id = R.string.input_field_card_password_label),
+        hint = stringResource(id = R.string.input_field_card_password_hint),
+        value = value,
+        onValueChanged = onValueChanged,
+        isInputPassword = true,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CardPasswordInputFieldPreview() {
+    CardPasswordInputField(
+        value = "",
+        onValueChanged = {},
+    )
+}
