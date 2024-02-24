0fcdf26eff3e557483735aac2348d536a2d26e03
강현석 <kekemusa37@gmail.com>
2024-02-24 23:50:17 +0900

    [구현] CardImage

--- app/src/main/java/nextstep/payments/screen/CardAddScreen.kt
+++ app/src/main/java/nextstep/payments/screen/CardAddScreen.kt
@@ -1,14 +1,21 @@
 package nextstep.payments.screen
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -18,6 +25,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,10 +49,61 @@ fun CardAddScreen(modifier: Modifier = Modifier) {
                     .padding(horizontal = 24.dp)
                     .verticalScroll(state = rememberScrollState())
             ) {
-
+                CardImage(modifier = Modifier.padding(top = 14.dp))
             }
         }
     )
+}
+
+@Composable
+private fun CardImage(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .width(width = 208.dp)
+                .height(height = 124.dp)
+                .drawBehind {
+                    drawIntoCanvas { canvas ->
+                        val paint = Paint()
+                        with(paint.asFrameworkPaint()) {
+                            color = Color.Black
+                                .copy(alpha = 0.25f)
+                                .toArgb()
+                            maskFilter = BlurMaskFilter(
+                                5.dp.toPx(),
+                                BlurMaskFilter.Blur.NORMAL
+                            )
+                        }
+                        canvas.drawRoundRect(
+                            left = 3.dp.toPx(),
+                            top = 3.dp.toPx(),
+                            right = size.width,
+                            bottom = size.height,
+                            radiusX = 4.dp.toPx(),
+                            radiusY = 4.dp.toPx(),
+                            paint = paint
+                        )
+                    }
+                }
+                .clip(shape = RoundedCornerShape(size = 4.dp))
+                .background(color = Color(0xff333333))
+                .padding(horizontal = 14.dp)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .padding(top = 44.dp)
+                    .width(width = 40.dp)
+                    .height(height = 26.dp)
+                    .clip(shape = RoundedCornerShape(size = 4.dp))
+                    .background(color = Color(0xFFCBBA64))
+            )
+        }
+    }
 }
 
 @Composable
