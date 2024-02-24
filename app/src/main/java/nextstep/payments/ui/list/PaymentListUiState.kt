02de8093197a01bc3d7db890e60cffcee501c049
laco-dev <laco2951@gmail.com>
2024-02-24 16:01:50 +0900

    목록 출력을 위한 UI 상태 및 뷰모델 로직 구현

---
+++ app/src/main/java/nextstep/payments/ui/list/PaymentListUiState.kt
@@ -0,0 +1,7 @@
+package nextstep.payments.ui.list
+
+import nextstep.payments.ui.domain.model.Card
+
+data class PaymentListUiState(
+    val cards: List<Card>,
+)
--- app/src/main/java/nextstep/payments/ui/list/PaymentListViewModel.kt
+++ app/src/main/java/nextstep/payments/ui/list/PaymentListViewModel.kt
@@ -1,5 +1,24 @@
 package nextstep.payments.ui.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import nextstep.payments.ui.domain.repository.CardRepository
 
-internal class PaymentListViewModel : ViewModel()
+internal class PaymentListViewModel(
+    private val cardRepository: CardRepository,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(PaymentListUiState(emptyList()))
+    val uiState: StateFlow<PaymentListUiState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val cards = cardRepository.getCardList()
+            _uiState.value = PaymentListUiState(cards)
+        }
+    }
+}
