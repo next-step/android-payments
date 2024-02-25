6c7908af843f30b4dbde129e174044c4bcfa53cb
malibinYun <nightmare73@naver.com>
2024-02-26 00:10:34 +0900

    CardsViewModel 추가

---
+++ app/src/main/java/nextstep/payments/card/CardsViewModel.kt
@@ -0,0 +1,37 @@
+package nextstep.payments.card
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import nextstep.payments.card.add.CardsScreenUiState
+
+class CardsViewModel(
+    private val cardRepository: CardRepository = DefaultCardRepository,
+) : ViewModel() {
+
+    private val _cardsScreenUiState: MutableStateFlow<CardsScreenUiState> =
+        MutableStateFlow(getCurrentCardsScreenUiState())
+    val cardsScreenUiState: StateFlow<CardsScreenUiState> = _cardsScreenUiState.asStateFlow()
+
+    fun updateCards() {
+        viewModelScope.launch {
+            _cardsScreenUiState.update { getCurrentCardsScreenUiState() }
+        }
+    }
+
+    private fun getCurrentCardsScreenUiState(): CardsScreenUiState {
+        return resolveCardsScreenUiState(cardRepository.getAllCards())
+    }
+
+    private fun resolveCardsScreenUiState(cards: List<Card>): CardsScreenUiState {
+        return when {
+            cards.isEmpty() -> CardsScreenUiState.Empty
+            cards.size == 1 -> CardsScreenUiState.SingleCard(cards[0])
+            else -> CardsScreenUiState.MultipleCards(cards)
+        }
+    }
+}
