8fffbe62dc286d2a17c0b0ec44ed76f09c7f2910
namjackson <skawo32167@naver.com>
2024-02-26 22:45:09 +0900

    Step2 : PaymentCards 화면 구현

---
+++ app/src/main/java/nextstep/payments/ui/payments/PaymentCardsScreen.kt
@@ -0,0 +1,78 @@
+package nextstep.payments.ui.payments
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.sp
+import nextstep.payments.R
+import nextstep.payments.domain.PaymentCard
+import nextstep.payments.ui.payments.component.PaymentCardList
+
+@Composable
+fun PaymentCardsScreenRoute(
+    onAddCardClick: () -> Unit
+) {
+    PaymentCardsScreen(
+        uiState = PaymentCardsUiState.One(PaymentCard.MockData),
+        onAddCardClick = onAddCardClick
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun PaymentCardsScreen(
+    uiState: PaymentCardsUiState,
+    onAddCardClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.payments_title),
+                        fontSize = 22.sp
+                    )
+                },
+                actions = {
+                    if (uiState is PaymentCardsUiState.Many) {
+                        TextButton(onClick = { onAddCardClick() }) {
+                            Text(
+                                text = stringResource(id = R.string.payments_add_card),
+                                color = Color.Black,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                },
+            )
+        }
+    ) { innerPadding ->
+        PaymentCardList(
+            modifier = Modifier.padding(innerPadding),
+            onAddCardClick = onAddCardClick,
+            uiState = uiState
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PaymentCardsScreenPreview(
+    @PreviewParameter(PaymentCardsUiStatePreviewParameterProvider::class) paymentCardsUiState: PaymentCardsUiState
+) {
+    PaymentCardsScreen(
+        uiState = paymentCardsUiState,
+        onAddCardClick = {}
+    )
+}
--- app/src/main/java/nextstep/payments/ui/payments/PaymentCardsUiState.kt
+++ app/src/main/java/nextstep/payments/ui/payments/PaymentCardsUiState.kt
@@ -1,9 +1,26 @@
 package nextstep.payments.ui.payments
 
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import nextstep.payments.domain.PaymentCard
 
 sealed interface PaymentCardsUiState {
     data object Empty : PaymentCardsUiState
     data class One(val card: PaymentCard) : PaymentCardsUiState
     data class Many(val cards: List<PaymentCard>) : PaymentCardsUiState
+}
+
+internal class PaymentCardsUiStatePreviewParameterProvider :
+    PreviewParameterProvider<PaymentCardsUiState> {
+    override val values = sequenceOf(
+        PaymentCardsUiState.Empty,
+        PaymentCardsUiState.One(PaymentCard.MockData),
+        PaymentCardsUiState.Many(
+            cards = listOf(
+                PaymentCard.MockData,
+                PaymentCard.MockData,
+                PaymentCard.MockData,
+                PaymentCard.MockData
+            )
+        )
+    )
 }
