9c855759f12fa9142748f142f027f428d03acb4b
Minjae Lee <stkment@gmail.com>
2024-08-22 00:24:30 +0900

    🚀 1단계 - 페이먼츠(카드 추가) (#18)
    * chore: reformat codes
    
    * test: LazyColumnTest 추가
    
    * feat: NewScreen 추가

--- app/src/main/java/nextstep/payments/NewCardActivity.kt
+++ app/src/main/java/nextstep/payments/NewCardActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import nextstep.payments.ui.newcard.NewCardRoute
 import nextstep.payments.ui.theme.PaymentsTheme
 
-class MainActivity : ComponentActivity() {
+class NewCardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,27 +18,11 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    NewCardRoute()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PaymentsTheme {
-        Greeting("Android")
     }
 }
