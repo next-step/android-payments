f8ffcb9a5f44b83c9d546234427303741062feb5
JINHYUK, JANG <ironelder@gmail.com>
2024-08-21 23:15:45 +0900

    Step1: 페이먼츠(카드추가) (#10)
    * Step1: LazyColumn Test 추가
    
    * Step1: ViewModel 추가
    
    * Step1: Card 이미지 생성
    
    * Step1: TopBar 추가
    
    * Step1: NewCardScreen 추가
    
    * Step1: MainActivity에 Screen 출력
    
    ---------
    
    Co-authored-by: 장진혁/StoreClient팀/원스토어 <ironelder@onestorecorp.com>

--- app/src/main/java/nextstep/payments/MainActivity.kt
+++ app/src/main/java/nextstep/payments/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import nextstep.payments.ui.screen.NewCardScreen
+import nextstep.payments.ui.screen.NewCardViewModel
 import nextstep.payments.ui.theme.PaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +24,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NewCardScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PaymentsTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/nextstep/payments/ui/screen/NewCardViewModel.kt
@@ -0,0 +1,37 @@
+package nextstep.payments.ui.screen
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class NewCardViewModel : ViewModel() {
+
+    private val _cardNumber = MutableStateFlow("")
+    val cardNumber: StateFlow<String> = _cardNumber.asStateFlow()
+
+    private val _expiredDate = MutableStateFlow("")
+    val expiredDate: StateFlow<String> = _expiredDate.asStateFlow()
+
+    private val _ownerName = MutableStateFlow("")
+    val ownerName: StateFlow<String> = _ownerName.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password.asStateFlow()
+
+    fun setCardNumber(cardNumber: String) {
+        _cardNumber.value = cardNumber
+    }
+
+    fun setExpiredDate(expiredDate: String) {
+        _expiredDate.value = expiredDate
+    }
+
+    fun setOwnerName(ownerName: String) {
+        _ownerName.value = ownerName
+    }
+
+    fun setPassword(password: String) {
+        _password.value = password
+    }
+}
