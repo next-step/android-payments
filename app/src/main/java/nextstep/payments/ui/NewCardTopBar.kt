c8254f37eb6f31509516fb4513b2988ef1842fcd
박규동 <kyudong3@gmail.com>
2024-08-21 22:32:34 +0900

    Step1 페이먼츠(카드 추가) (#16)
    * docs: step1 Readme 추가
    
    * feat: 카드 추가 화면 구현

---
+++ app/src/main/java/nextstep/payments/ui/NewCardTopBar.kt
@@ -0,0 +1,46 @@
+package nextstep.payments.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import nextstep.payments.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewCardTopBar(
+    onBackClick: () -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = {
+            Text(stringResource(id = R.string.add_card))
+        },
+        navigationIcon = {
+            IconButton(onClick = { onBackClick() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(id = R.string.go_back),
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { onSaveClick() }) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = stringResource(id = R.string.complete),
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
