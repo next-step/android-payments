5f3d7b091c0556dc5fa83d1368e1c55333025da7
jay.lee <stkment@gmail.com>
2024-08-22 01:27:35 +0900

    feat: NewCarUiState 추가

--- app/src/main/java/nextstep/payments/ui/newcard/NewCardViewModel.kt
+++ app/src/main/java/nextstep/payments/ui/newcard/NewCardViewModel.kt
@@ -4,33 +4,33 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import nextstep.payments.model.OwnerNameValidResult
 
 class NewCardViewModel : ViewModel() {
-    private val _cardNumber = MutableStateFlow("")
-    val cardNumber: StateFlow<String> = _cardNumber.asStateFlow()
-
-    private val _expiredDate = MutableStateFlow("")
-    val expiredDate: StateFlow<String> = _expiredDate.asStateFlow()
-
-    private val _ownerName = MutableStateFlow("")
-    val ownerName: StateFlow<String> = _ownerName.asStateFlow()
-
-    private val _password = MutableStateFlow("")
-    val password: StateFlow<String> = _password.asStateFlow()
+    private val _uiState = MutableStateFlow(NewCardScreenUiState.NONE)
+    val uiState: StateFlow<NewCardScreenUiState> = _uiState.asStateFlow()
 
     fun setCardNumber(cardNumber: String) {
-        _cardNumber.value = cardNumber
+        _uiState.update { it.copy(cardNumber = cardNumber) }
     }
 
     fun setExpiredDate(expiredDate: String) {
-        _expiredDate.value = expiredDate
+        _uiState.update { it.copy(expiredDate = expiredDate) }
     }
 
     fun setOwnerName(ownerName: String) {
-        _ownerName.value = ownerName
+        val result = validateOwnerName(ownerName)
+        _uiState.update { it.copy(ownerName = ownerName, ownerNameValidResult = result) }
     }
 
+    private fun validateOwnerName(ownerName: String): OwnerNameValidResult =
+        when {
+            ownerName.length > 30 -> OwnerNameValidResult.ERROR_OWNER_NAME_LENGTH
+            else -> OwnerNameValidResult.VALID
+        }
+
     fun setPassword(password: String) {
-        _password.value = password
+        _uiState.update { it.copy(password = password) }
     }
 }
