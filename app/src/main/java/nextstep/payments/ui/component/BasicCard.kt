28105277eeac869dd75ea63fd5e8212dad2efee3
ezhoon <dlwlgns1240@gmail.com>
2024-08-22 21:25:51 +0900

    feat: PaymentCard에서 공통화된 UI를 BasicCard로 분리

---
+++ app/src/main/java/nextstep/payments/ui/component/BasicCard.kt
@@ -0,0 +1,40 @@
+package nextstep.payments.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BasicCard(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = Color(0xFF333333),
+    content: @Composable BoxScope.() -> Unit
+) {
+    Box(
+        modifier = modifier
+            .shadow(8.dp)
+            .size(width = 208.dp, height = 124.dp)
+            .background(
+                color = backgroundColor,
+                shape = RoundedCornerShape(5.dp),
+            )
+            .padding(horizontal = 14.dp)
+    ) {
+        content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BasicCardPreview() {
+    BasicCard {}
+}
--- app/src/main/java/nextstep/payments/ui/component/PaymentCard.kt
+++ app/src/main/java/nextstep/payments/ui/component/PaymentCard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,50 +29,45 @@ fun PaymentCard(
     cardOwnerName: String,
     cardExpiredDate: String,
 ) {
-    Column(
-        verticalArrangement = Arrangement.Bottom,
-        modifier = modifier
-            .shadow(8.dp)
-            .size(width = 208.dp, height = 124.dp)
-            .background(
-                color = Color(0xFF333333),
-                shape = RoundedCornerShape(5.dp),
-            )
-            .padding(horizontal = 14.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .padding(bottom = 10.dp)
-                .size(width = 40.dp, height = 26.dp)
-                .background(
-                    color = Color(0xFFCBBA64),
-                    shape = RoundedCornerShape(4.dp),
-                )
-        )
-
-        CompositionLocalProvider(
-            LocalContentColor provides Color.White,
-            LocalTextStyle provides MaterialTheme.typography.labelMedium
+    BasicCard(modifier = modifier) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
         ) {
-            CardNumberText(
-                modifier = Modifier.fillMaxWidth(),
-                cardNumber = cardNumber,
+            Box(
+                modifier = Modifier
+                    .padding(bottom = 10.dp)
+                    .size(width = 40.dp, height = 26.dp)
+                    .background(
+                        color = Color(0xFFCBBA64),
+                        shape = RoundedCornerShape(4.dp),
+                    )
             )
 
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp, top = 2.dp)
+            CompositionLocalProvider(
+                LocalContentColor provides Color.White,
+                LocalTextStyle provides MaterialTheme.typography.labelMedium
             ) {
-                Text(
-                    text = cardOwnerName,
-                    color = Color.White,
+                CardNumberText(
+                    modifier = Modifier.fillMaxWidth(),
+                    cardNumber = cardNumber,
                 )
-                Text(
-                    text = cardExpiredDate,
-                    color = Color.White,
-                )
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp, top = 2.dp)
+                ) {
+                    Text(
+                        text = cardOwnerName,
+                        color = Color.White,
+                    )
+                    Text(
+                        text = cardExpiredDate,
+                        color = Color.White,
+                    )
+                }
             }
         }
     }
