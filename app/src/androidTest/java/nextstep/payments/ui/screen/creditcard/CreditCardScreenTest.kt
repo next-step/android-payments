309184588c386fa84535692085c530054af8d019
ezhoon <dlwlgns1240@gmail.com>
2024-08-22 23:48:53 +0900

    refactor: 상단바에 대한 클릭 이벤트 테스트 코드 추가

--- app/src/androidTest/java/nextstep/payments/ui/screen/creditcard/CreditCardScreenTest.kt
+++ app/src/androidTest/java/nextstep/payments/ui/screen/creditcard/CreditCardScreenTest.kt
@@ -2,6 +2,8 @@ package nextstep.payments.ui.screen.creditcard
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import nextstep.payments.BaseComposeTest
 import nextstep.payments.R
 import nextstep.payments.ui.screen.creditcard.model.CreditCard
@@ -11,6 +13,7 @@ import org.junit.Test
 internal class CreditCardScreenTest : BaseComposeTest() {
 
     private val state = mutableStateOf(CreditCardUiState(cards = emptyList()))
+    private var onAddClickCount = 0
 
     private val emptySectionDescription = resourceTestRule.getString(R.string.credit_card_section_zero)
     private val oneItemSectionDescription = resourceTestRule.getString(R.string.credit_card_section_one)
@@ -20,8 +23,9 @@ internal class CreditCardScreenTest : BaseComposeTest() {
 
     @Before
     fun setUp() {
+        onAddClickCount = 0
         composeTestRule.setContent {
-            CreditCardScreen(state = state.value, onAddClick = { /*TODO*/ })
+            CreditCardScreen(state = state.value, onAddClick = { onAddClickCount += 1 })
         }
     }
 
@@ -36,7 +40,7 @@ internal class CreditCardScreenTest : BaseComposeTest() {
             .onNodeWithContentDescription(creditCardAddDescription)
             .assertExists()
         composeTestRule
-            .onNodeWithContentDescription(creditCardAddActionText)
+            .onNodeWithText(creditCardAddActionText)
             .assertDoesNotExist()
     }
 
@@ -59,14 +63,14 @@ internal class CreditCardScreenTest : BaseComposeTest() {
             .onNodeWithContentDescription(creditCardAddDescription)
             .assertExists()
         composeTestRule
-            .onNodeWithContentDescription(creditCardAddActionText)
+            .onNodeWithText(creditCardAddActionText)
             .assertDoesNotExist()
     }
 
     @Test
-    fun 카드가_2개_이상일_때_카드들만_보여야_하고_추가_버튼이_없어야_한다() {
+    fun 카드가_2개_이상일_때_카드들만_보여야_하고_추가_버튼이_있어야_한다() {
         state.value = CreditCardUiState(
-            cards = List(2) {
+            cards = List(3) {
                 CreditCard(
                     cardNumber = "1111222233334444",
                     cardOwnerName = "이지훈",
@@ -82,7 +86,37 @@ internal class CreditCardScreenTest : BaseComposeTest() {
             .onNodeWithContentDescription(creditCardAddDescription)
             .assertDoesNotExist()
         composeTestRule
-            .onNodeWithContentDescription(creditCardAddActionText)
-            .assertDoesNotExist()
+            .onNodeWithText(creditCardAddActionText)
+            .assertExists()
+    }
+
+    @Test
+    fun 상단_바에_카드_추가_버튼을_누르면_카드_추가_화면으로_이동하는_로직이_실행된다() {
+        state.value = CreditCardUiState(
+            cards = List(3) {
+                CreditCard(
+                    cardNumber = "1111222233334444",
+                    cardOwnerName = "이지훈",
+                    cardExpiredDate = "22 / 33"
+                )
+            }
+        )
+
+        composeTestRule
+            .onNodeWithText(creditCardAddActionText)
+            .performClick()
+
+        assert(onAddClickCount == 1)
+    }
+
+    @Test
+    fun 카드_목록의_추가_버튼을_누르면_카드_추가_화면으로_이동하는_로직이_실행된다() {
+        state.value = CreditCardUiState(cards = emptyList())
+
+        composeTestRule
+            .onNodeWithContentDescription(creditCardAddDescription)
+            .performClick()
+
+        assert(onAddClickCount == 1)
     }
 }
