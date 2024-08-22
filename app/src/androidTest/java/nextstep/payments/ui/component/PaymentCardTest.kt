30971be7064c8bad4c13980b7946f84188d7f5be
jay.lee <stkment@gmail.com>
2024-08-23 01:47:07 +0900

    test: 테스트 코드 정리 및 간소화

--- app/src/androidTest/java/nextstep/payments/ui/component/PaymentCardTest.kt
+++ app/src/androidTest/java/nextstep/payments/ui/component/PaymentCardTest.kt
@@ -1,26 +1,30 @@
 package nextstep.payments.ui.component
 
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import nextstep.payments.model.Brand
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class PaymentCardTest {
     @get:Rule
     val composeTestRule = createComposeRule()
-    private var cardNumber by mutableStateOf("")
-    private var expiredDate by mutableStateOf("")
-    private var ownerName by mutableStateOf("")
-    private var brand by mutableStateOf(Brand.NONE)
 
-    @Before
-    fun setUp() {
+    @Test
+    fun PaymentCard_화면이_정상적으로_렌더링된다() {
+        // given
+        val cardNumber = "1234567890123456"
+        val expiredDate = "1234"
+        val ownerName = "홍길동"
+        val brand = Brand.SHINHAN
+
+        val expectedCardNumber = "1234  -  5678  -  ****  -  ****"
+        val expectedExpiredDated = "12 / 34"
+        val expectedBrandName = "신한카드"
+
         composeTestRule.setContent {
             PaymentCard(
                 brand = brand,
@@ -29,19 +33,6 @@ class PaymentCardTest {
                 ownerName = ownerName,
             )
         }
-    }
-
-    @Test
-    fun PaymentCard_화면이_정상적으로_렌더링된다() {
-        // given
-        cardNumber = "1234567890123456"
-        expiredDate = "1234"
-        ownerName = "홍길동"
-        brand = Brand.SHINHAN
-
-        val expectedCardNumber = "1234  -  5678  -  ****  -  ****"
-        val expectedExpiredDated = "12 / 34"
-        val expectedBrandName = "신한카드"
 
         // when & then
         composeTestRule
