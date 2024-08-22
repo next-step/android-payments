11fed3a13ea3f4880d95f53e6d45403ea0d011bc
dino <sjjeong1225@gmail.com>
2024-08-22 16:31:12 +0900

    feat: 카드등록 component 추가

--- app/src/main/java/nextstep/payments/component/PaymentCardRegister.kt
+++ app/src/main/java/nextstep/payments/component/PaymentCardRegister.kt
@@ -1,13 +1,17 @@
-package nextstep.payments.ui
+package nextstep.payments.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,33 +19,28 @@ import androidx.compose.ui.unit.dp
 import nextstep.payments.consts.CardConstant
 
 @Composable
-fun PaymentCard(
+fun PaymentCardRegister(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Box(
-        contentAlignment = Alignment.CenterStart,
         modifier = modifier
             .shadow(8.dp)
             .size(width = CardConstant.CARD_WIDTH.dp, height = CardConstant.CARD_HEIGHT.dp)
-            .background(
-                color = Color(0xFF333333),
-                shape = RoundedCornerShape(5.dp),
-            )
+            .clip(RoundedCornerShape(5.dp))
+            .background(color = Color(0xFFE5E5E5))
+            .clickable { onClick() }
     ) {
-        Box(
-            modifier = Modifier
-                .padding(start = 14.dp, bottom = 10.dp)
-                .size(width = 40.dp, height = 26.dp)
-                .background(
-                    color = Color(0xFFCBBA64),
-                    shape = RoundedCornerShape(4.dp),
-                )
+        Icon(
+            modifier = Modifier.align(Alignment.Center),
+            imageVector = Icons.Filled.Add,
+            contentDescription = "카드 추가 아이콘"
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun PaymentCardPreview() {
-    PaymentCard()
+private fun PaymentCardRegisterPreview() {
+    PaymentCardRegister(onClick = {})
 }
---
+++ app/src/main/java/nextstep/payments/consts/CardConstant.kt
@@ -0,0 +1,6 @@
+package nextstep.payments.consts
+
+object CardConstant {
+    const val CARD_WIDTH = 208
+    const val CARD_HEIGHT = 124
+}
