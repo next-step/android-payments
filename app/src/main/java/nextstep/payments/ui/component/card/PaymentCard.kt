2da61554fb064b70e079e66c90fa1eb1673ca6f4
ezhoon <dlwlgns1240@gmail.com>
2024-08-23 13:05:45 +0900

    refactor: PaymentCard fillMaxSize -> BoxScope.align 수정

--- app/src/main/java/nextstep/payments/ui/component/card/PaymentCard.kt
+++ app/src/main/java/nextstep/payments/ui/component/card/PaymentCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,7 +34,7 @@ fun PaymentCard(
 ) {
     BasicCard(modifier = modifier) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.align(Alignment.BottomStart).fillMaxWidth(),
             verticalArrangement = Arrangement.Bottom,
         ) {
             Box(
