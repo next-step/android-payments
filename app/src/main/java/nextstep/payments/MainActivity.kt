b749a15cf1abe54c75abc68bc130ddf0ee50da15
OYJ <89826723+oyj7677@users.noreply.github.com>
2024-08-23 10:18:05 +0900

    Step1 - 페이먼츠(카드 추가) (#25)

--- app/src/main/java/nextstep/payments/MainActivity.kt
+++ app/src/main/java/nextstep/payments/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import nextstep.payments.ui.NewCardScreen
 import nextstep.payments.ui.theme.PaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NewCardScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PaymentsTheme {
-        Greeting("Android")
-    }
-}
