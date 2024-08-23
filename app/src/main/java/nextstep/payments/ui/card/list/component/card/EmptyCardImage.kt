f308088b818994fbe05a463f4db0176db92e6822
OYJ <oyj7677@gmail.com>
2024-08-24 04:03:01 +0900

    Feat : 카드 등록 엑티비티 이동 구현
    1. 빈 카드 이미지 클릭 시 카드 등록 엑티비티 이동

--- app/src/main/java/nextstep/payments/ui/card/list/component/card/EmptyCardImage.kt
+++ app/src/main/java/nextstep/payments/ui/card/list/component/card/EmptyCardImage.kt
@@ -1,5 +1,7 @@
 package nextstep.payments.ui.card.list.component.card
 
+import android.content.Intent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
@@ -12,18 +14,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nextstep.payments.ui.card.registration.NewCardActivity
 import nextstep.payments.ui.theme.PaymentsTheme
 
 @Composable
 fun EmptyCardImage(cardColor: Color, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     Card(
-        modifier = modifier,
+        modifier = modifier
+            .clickable {
+                val intent = Intent(context, NewCardActivity::class.java).apply {
+                }
+                context.startActivity(intent)
+            },
         colors = CardDefaults.cardColors(
             containerColor = cardColor,
         )
-
     ) {
         Box(
             modifier = Modifier.fillMaxSize(),
