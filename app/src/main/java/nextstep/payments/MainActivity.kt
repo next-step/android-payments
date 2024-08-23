ca7292b075a502279c3c3cb9a5af7c14f6645747
Yongho Choi <73802331+yongho2157@users.noreply.github.com>
2024-08-23 10:12:56 +0900

    feat: 1단계 - 페이먼츠(카드 추가) (#23)

--- app/src/main/java/nextstep/payments/MainActivity.kt
+++ app/src/main/java/nextstep/payments/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import nextstep.payments.ui.NewCardScreen
+import nextstep.payments.ui.PaymentCard
 import nextstep.payments.ui.theme.PaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NewCardScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PaymentsTheme {
-        Greeting("Android")
     }
 }
