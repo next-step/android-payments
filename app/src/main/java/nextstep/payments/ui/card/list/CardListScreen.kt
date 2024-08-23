bdf1c1e85262ee9009375980c42a1fec26a84cf6
OYJ <oyj7677@gmail.com>
2024-08-23 16:27:16 +0900

    Feat : 카드 리스트 탑바 생성
    1. CardListTopBar 컴포넌트 생성
    2. CardListScreen의 Scaffold(topBar) 에 적용
    3. LazyColumn 세팅

--- app/src/main/java/nextstep/payments/ui/card/list/CardListScreen.kt
+++ app/src/main/java/nextstep/payments/ui/card/list/CardListScreen.kt
@@ -1,9 +1,31 @@
 package nextstep.payments.ui.card.list
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import nextstep.payments.ui.card.list.component.CardListTopBar
+import nextstep.payments.ui.theme.PaymentsTheme
 
 @Composable
 fun CardListScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = { CardListTopBar() }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues)
+        ) {
 
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CardListScreenPreview() {
+    PaymentsTheme {
+        CardListScreen()
+    }
 }
