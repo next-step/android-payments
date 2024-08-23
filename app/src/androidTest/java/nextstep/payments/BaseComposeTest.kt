6976e15cff1ef6c5e3d820d3123da193fb03d5a8
ezhoon <dlwlgns1240@gmail.com>
2024-08-23 18:57:50 +0900

    refactor: BaseComposeTest open -> abstract 변경

--- app/src/androidTest/java/nextstep/payments/BaseComposeTest.kt
+++ app/src/androidTest/java/nextstep/payments/BaseComposeTest.kt
@@ -3,7 +3,7 @@ package nextstep.payments
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Rule
 
-open class BaseComposeTest {
+abstract class BaseComposeTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
