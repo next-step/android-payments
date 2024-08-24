dff707e6650aadfd3ce8509f711882b6935bc25f
sim junbo <46774431+simuelunbo@users.noreply.github.com>
2024-08-24 17:26:49 +0900

    step1: step1 Payments UI 구현 (#15)

--- app/src/main/java/nextstep/payments/MainActivity.kt
+++ app/src/main/java/nextstep/payments/MainActivity.kt
@@ -17,13 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PaymentsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                NewCardScreen()
             }
         }
     }
