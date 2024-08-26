5478ab6ea3737f042e0355c994f5e83e9fd83d23
ezhoon <dlwlgns1240@gmail.com>
2024-08-26 19:31:39 +0900

    chore: 불필요한 import 제거

--- app/src/androidTest/java/nextstep/payments/ui/component/ComponentTests.kt
+++ app/src/androidTest/java/nextstep/payments/ui/component/ComponentTests.kt
@@ -1,7 +1,5 @@
 package nextstep.payments.ui.component
 
-import nextstep.payments.ui.screen.creditcard.CreditCardScreenTest
-import nextstep.payments.ui.screen.newcard.NewCardScreenTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import org.junit.runners.Suite.SuiteClasses
