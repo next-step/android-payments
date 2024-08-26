ebaac537965bbe6cfe89e3f7b664f8de9b5b9e1f
OYJ <oyj7677@gmail.com>
2024-08-26 14:25:48 +0900

    Refactor : 빈 카드 이미지 버튼 클릭 영역 수정
    1. clip(shape = CardDefaults.shape) 속성을 추가하여 둥근 모서리에 맞게 수정

--- app/src/main/java/nextstep/payments/ui/card/list/component/card/EmptyCardImage.kt
+++ app/src/main/java/nextstep/payments/ui/card/list/component/card/EmptyCardImage.kt
@@ -9,11 +9,12 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nextstep.payments.ui.theme.PaymentsTheme
@@ -22,6 +23,7 @@ import nextstep.payments.ui.theme.PaymentsTheme
 fun EmptyCardImage(cardColor: Color, onAddCard: () -> Unit, modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
+            .clip(shape = CardDefaults.shape)
             .clickable { onAddCard() },
         colors = CardDefaults.cardColors(
             containerColor = cardColor,
