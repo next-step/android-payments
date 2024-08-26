c71c8c37e8787586983454d131a361d6c76d8fe1
eshc123 <>
2024-08-27 05:54:12 +0900

    feat: 카드 목록 화면 구현

--- app/src/main/java/nextstep/payments/data/model/Card.kt
+++ app/src/main/java/nextstep/payments/data/model/Card.kt
@@ -5,4 +5,11 @@ data class Card(
     val expiredDate : String,
     val ownerName : String,
     val password : String
-)
+){
+    private val cardNumberList : List<String> by lazy {
+        cardNumber.split(" - ")
+    }
+
+    val firstCardDigits = cardNumberList.firstOrNull() ?: ""
+    val secondCardDigits = cardNumberList.getOrNull(2) ?: ""
+}
---
+++ app/src/main/java/nextstep/payments/screen/CardListScreen.kt
@@ -0,0 +1,158 @@
+package nextstep.payments.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.payments.R
+import nextstep.payments.component.card.AdditionCard
+import nextstep.payments.component.card.PaymentCard
+import nextstep.payments.component.topbar.CardListTopBar
+import nextstep.payments.data.model.Card
+import nextstep.payments.ui.theme.PaymentsTheme
+
+@Composable
+fun CardListScreen(
+    modifier: Modifier = Modifier
+) {
+    CardListScreen(
+        modifier = modifier,
+        cardList = emptyList()
+    )
+}
+
+@Composable
+fun CardListScreen(
+    cardList: List<Card>,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CardListTopBar(
+                onSaveClick = { /*TODO*/ },
+                isShownAddText = cardList.size > 3
+            )
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentPadding = PaddingValues(vertical = 32.dp),
+            verticalArrangement = Arrangement.spacedBy(36.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if(cardList.isEmpty()){
+                item {
+                    AdditionCardText()
+                }
+            }
+            if(cardList.isNotEmpty()){
+                items(cardList){ card ->
+                    PaymentCard(
+                        cardNumber = stringResource(id = R.string.card_number,card.firstCardDigits,card.secondCardDigits),
+                        ownerName = card.ownerName,
+                        expiredDate = card.expiredDate
+                    )
+                }
+            }
+            if(cardList.size <= 3){
+                item {
+                    AdditionCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AdditionCardText(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(id = R.string.card_list_item_add),
+        style = MaterialTheme.typography.titleMedium,
+        color = Color(0xFF333333)
+    )
+}
+
+@Preview(showBackground = true, name = "CardListScreen")
+@Composable
+private fun Preview1() {
+    PaymentsTheme {
+        CardListScreen()
+    }
+}
+
+@Preview(showBackground = true, name = "CardListScreenWithACard")
+@Composable
+private fun Preview2() {
+    PaymentsTheme {
+        CardListScreen(
+            cardList = listOf(
+                Card(
+                    cardNumber = "1234 - 1234 - 1234 - 1234",
+                    ownerName = "CREW",
+                    expiredDate = "04 / 21",
+                    password = "1234"
+                )
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "CardListScreenWithCards")
+@Composable
+private fun Preview3() {
+    PaymentsTheme {
+        CardListScreen(
+            cardList = listOf(
+                Card(
+                    cardNumber = "1234 - 1234 - 1234 - 1234",
+                    ownerName = "CREW",
+                    expiredDate = "04 / 21",
+                    password = "1234"
+                ),
+                Card(
+                    cardNumber = "1234 - 1234 - 1234 - 1234",
+                    ownerName = "CREW1",
+                    expiredDate = "04 / 21",
+                    password = "1234"
+                ),
+                Card(
+                    cardNumber = "1234 - 1234 - 1234 - 1234",
+                    ownerName = "CREW2",
+                    expiredDate = "04 / 21",
+                    password = "1234"
+                ),
+                Card(
+                    cardNumber = "1234 - 1234 - 1234 - 1234",
+                    ownerName = "CREW3",
+                    expiredDate = "04 / 21",
+                    password = "1234"
+                )
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "AdditionCardText")
+@Composable
+private fun Preview4() {
+    PaymentsTheme {
+        AdditionCardText()
+    }
+}
