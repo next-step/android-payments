e70f68808d693450b7b22994748b4cd2bc5e3812
junbo sim <junbo.sim@lemonhc.com>
2024-08-27 15:05:53 +0900

    feat: repository, card model 추가

---
+++ app/src/main/java/nextstep/payments/model/Card.kt
@@ -0,0 +1,10 @@
+package nextstep.payments.model
+
+import java.time.YearMonth
+
+data class Card(
+    val cardNumber: String,
+    val expiredDate: String,
+    val ownerName: String,
+    val password: String,
+)
