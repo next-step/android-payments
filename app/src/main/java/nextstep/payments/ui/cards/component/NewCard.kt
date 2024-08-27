b522641a401009115a583b32e528e6f628f900c0
junbo sim <junbo.sim@lemonhc.com>
2024-08-27 16:49:04 +0900

    feat: CardList 카드 UI 컴포넌트 추가

---
+++ app/src/main/java/nextstep/payments/ui/cards/component/NewCard.kt
@@ -0,0 +1,62 @@
+package nextstep.payments.ui.cards.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NewCard(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .size(
+                width = 208.dp,
+                height = 124.dp
+            )
+            .clip(
+                shape = RoundedCornerShape(5.dp)
+            )
+            .background(
+                color = Color(0xFFE5E5E5),
+            ),
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "addCard",
+                modifier = Modifier.align(Alignment.Center),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun NewCardPreview() {
+    NewCard(
+        modifier = Modifier
+            .width(208.dp)
+            .height(124.dp),
+        onClick = {},
+    )
+}
--- app/src/main/java/nextstep/payments/ui/common/PaymentsCard.kt
+++ app/src/main/java/nextstep/payments/ui/common/PaymentsCard.kt
@@ -1,39 +1,117 @@
 package nextstep.payments.ui.common
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.payments.model.Card
 
 @Composable
 fun PaymentCard(
     modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit = {}
 ) {
     Box(
-        contentAlignment = Alignment.CenterStart,
-        modifier = modifier
+        modifier
             .shadow(8.dp)
             .size(width = 208.dp, height = 124.dp)
             .background(
                 color = Color(0xFF333333),
                 shape = RoundedCornerShape(5.dp),
-            )
+            ),
+        Alignment.CenterStart
     ) {
         Box(
             modifier = Modifier
-                .padding(start = 14.dp, bottom = 10.dp)
-                .size(width = 40.dp, height = 26.dp)
+                .padding(
+                    start = 14.dp,
+                    bottom = 10.dp
+                )
+                .size(
+                    width = 40.dp,
+                    height = 26.dp
+                )
                 .background(
                     color = Color(0xFFCBBA64),
                     shape = RoundedCornerShape(4.dp),
                 )
         )
+        content()
     }
+}
+
+@Composable
+fun PaymentCard(
+    modifier: Modifier = Modifier,
+    card: Card
+) {
+    PaymentCard(modifier) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(14.dp),
+        ) {
+            Text(
+                text = card.cardNumber,
+                color = Color.White,
+                fontWeight = FontWeight.Medium,
+                fontSize = 12.sp,
+                lineHeight = 14.sp,
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = card.ownerName,
+                    color = Color.White,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 12.sp,
+                    lineHeight = 14.sp,
+                )
+                Text(
+                    text = card.expiredDate,
+                    color = Color.White,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 12.sp,
+                    lineHeight = 14.sp,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PaymentCardWithInfoEmptyPreview() {
+    PaymentCard()
+}
+
+@Preview
+@Composable
+private fun PaymentCardWithInfoPreview() {
+    PaymentCard(
+        card = Card(
+            cardNumber = "0000-0000-0000-0000",
+            expiredDate = "00 / 00",
+            ownerName = "컴포즈",
+            password = "0000",
+        ),
+    )
 }
