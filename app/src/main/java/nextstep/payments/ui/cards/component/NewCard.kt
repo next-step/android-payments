cb5fb4932f9ed3f350c47722233dc2e47fb9b248
simjunbo <simuelunbo@gmail.com>
2024-08-31 03:32:42 +0900

    refactor: 카드 추가 UI 컴포넌트 비율처리로 변경

--- app/src/main/java/nextstep/payments/ui/cards/component/NewCard.kt
+++ app/src/main/java/nextstep/payments/ui/cards/component/NewCard.kt
@@ -2,8 +2,12 @@ package nextstep.payments.ui.cards.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,21 +22,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun NewCard(
-    modifier: Modifier = Modifier,
     onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    aspectRatio: Float = 208f / 124f,
+    minWidth: Dp = 208.dp,
+    minHeight: Dp = 124.dp,
 ) {
     Card(
         onClick = onClick,
         shape = RoundedCornerShape(5.dp),
         modifier = modifier
-            .size(
-                width = 208.dp,
-                height = 124.dp
-            ),
+            .aspectRatio(aspectRatio)
+            .defaultMinSize(
+            minWidth = minWidth,
+            minHeight = minHeight
+        ),
         colors = CardDefaults.cardColors(
             containerColor = Color(0xFFE5E5E5)
         )
@@ -53,7 +62,7 @@ fun NewCard(
 @Composable
 private fun NewCardPreview() {
     NewCard(
-        modifier = Modifier
+        modifier = Modifier.padding(top = 32.dp)
             .width(208.dp)
             .height(124.dp),
         onClick = {},
