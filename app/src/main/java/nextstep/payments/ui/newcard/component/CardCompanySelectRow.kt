b0d3251992fa43cbfc68aac4c74af616fbf7c7df
simjunbo <simuelunbo@gmail.com>
2024-09-01 02:14:57 +0900

    feat: 카드사 선택 바텀시트 UI 컴포넌트 추가

---
+++ app/src/main/java/nextstep/payments/ui/newcard/component/CardCompanySelectRow.kt
@@ -0,0 +1,73 @@
+package nextstep.payments.ui.newcard.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.payments.model.CardCompany
+import nextstep.payments.ui.theme.PaymentsTheme
+
+private const val COLUMN_COUNT = 4
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun CardCompanySelectRow(
+    cardCompanies: List<CardCompany>,
+    onClickCardCompany: (CardCompany) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxWidth()
+    )
+    FlowRow(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 36.dp, horizontal = 48.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        maxItemsInEachRow = COLUMN_COUNT
+    ) {
+        cardCompanies.forEach { cardCompany ->
+            Column(
+                modifier = Modifier.clickable { onClickCardCompany(cardCompany) },
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painterResource(id = cardCompany.iconResource),
+                    contentDescription = "${cardCompany.companyName}Image",
+                    modifier = Modifier.size(37.dp),
+                )
+
+                Text(
+                    text = cardCompany.companyName,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 8.dp),
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CardCompanySelectRowPreview() {
+    PaymentsTheme {
+        CardCompanySelectRow(cardCompanies = CardCompany.activeCards, onClickCardCompany = {})
+    }
+}
+
---
+++ app/src/main/java/nextstep/payments/ui/newcard/component/CardSelectBottomSheet.kt
@@ -0,0 +1,44 @@
+package nextstep.payments.ui.newcard.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetProperties
+import androidx.compose.material3.SheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.SecureFlagPolicy
+import nextstep.payments.model.CardCompany
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardSelectBottomSheet(
+    sheetState: SheetState,
+    cardCompanies: List<CardCompany>,
+    onCardClick: (CardCompany) -> Unit,
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    ModalBottomSheet(
+        containerColor = Color.White,
+        sheetState = sheetState,
+        onDismissRequest = onDismissRequest,
+        properties = ModalBottomSheetProperties(
+            securePolicy = SecureFlagPolicy.Inherit,
+            isFocusable = true,
+            shouldDismissOnBackPress = false
+        )
+    ) {
+        Box(modifier = Modifier.padding(bottom = 70.dp)) {
+            CardCompanySelectRow(
+                modifier = modifier,
+                cardCompanies = cardCompanies,
+                onClickCardCompany = onCardClick,
+            )
+        }
+    }
+}
