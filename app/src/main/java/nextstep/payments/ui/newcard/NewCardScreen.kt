732f2717da6668c70b6a96c893697265e66e895d
bmsk <vv99911@gmail.com>
2024-09-06 15:04:33 +0900

    refact: 카드 추가할 때 뒤로가기 키 방지

--- app/src/main/java/nextstep/payments/ui/newcard/NewCardScreen.kt
+++ app/src/main/java/nextstep/payments/ui/newcard/NewCardScreen.kt
@@ -1,5 +1,6 @@
 package nextstep.payments.ui.newcard
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -9,6 +10,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetDefaults
+import androidx.compose.material3.ModalBottomSheetProperties
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -107,6 +110,9 @@ internal fun NewCardScreen(
                 contentDescription = "BankBottomSheet"
             },
             onDismissRequest = { },
+            properties = ModalBottomSheetDefaults.properties(
+                shouldDismissOnBackPress = false,
+            ),
             sheetState = bottomSheetState,
         ) {
             BankBottomSheetContent(
