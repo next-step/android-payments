f82878dd1a95a94b8bf0204f77e9507d5b79d8ab
simjunbo <simuelunbo@gmail.com>
2024-09-08 19:03:10 +0900

    feat: Parcelize 추가

--- app/src/main/java/nextstep/payments/model/Card.kt
+++ app/src/main/java/nextstep/payments/model/Card.kt
@@ -1,11 +1,13 @@
 package nextstep.payments.model
 
-import java.time.YearMonth
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Card(
     val cardNumber: String,
     val expiredDate: String,
     val ownerName: String,
     val password: String,
     val cardCompany: CardCompany?
-)
+) : Parcelable
