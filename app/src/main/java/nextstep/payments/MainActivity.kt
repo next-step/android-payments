945445ffccf565f4df540c2e1b2ef192ff58d0db
s9hn <tnghqn@naver.com>
2024-09-10 18:03:00 +0900

    feat: CardListScreen 구현

--- app/src/main/java/nextstep/payments/MainActivity.kt
+++ app/src/main/java/nextstep/payments/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import nextstep.payments.ui.AppNavHost
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
             PaymentsTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
+                    color = Color.White,
                 ) {
                     AppNavHost(navController = rememberNavController())
                 }
---
+++ app/src/main/java/nextstep/payments/ui/cardlist/CardListScreen.kt
@@ -0,0 +1,63 @@
+package nextstep.payments.ui.cardlist
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.payments.R
+import nextstep.payments.data.Card
+import nextstep.payments.ui.cardlist.component.CardListLazyColumn
+import nextstep.payments.ui.cardlist.component.CardListTopAppBar
+import nextstep.payments.ui.theme.RobotoBold
+
+@Composable
+fun CardListScreen(
+    cards: List<Card>,
+    onAddCardClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        CardListTopAppBar(onAddCardClick = onAddCardClick)
+        Spacer(modifier = Modifier.size(14.dp))
+
+        when (cards.isEmpty()) {
+            true -> Text(
+                text = stringResource(R.string.cardlist_text_no_card),
+                fontFamily = RobotoBold,
+                fontSize = 18.sp,
+            )
+
+            false -> CardListLazyColumn(cards)
+        }
+        Spacer(modifier = Modifier.weight(weight = 1f))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CardListScreenPreview() {
+    CardListScreen(
+        cards = listOf(
+            Card(
+                cardId = 0L,
+                cardNumber = "",
+                expiredDate = "",
+                ownerName = "",
+                password = ""
+            )
+        ),
+        onAddCardClick = { },
+    )
+    // 매개변수 3가지, 빈 리스트, 2개일때, 안보일때,
+}
