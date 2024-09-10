648577501f07d873d7eef7fc6d290413cde1288c
s9hn <tnghqn@naver.com>
2024-09-10 17:48:31 +0900

    feat: 폰트 추가

--- app/src/main/java/nextstep/payments/ui/theme/Type.kt
+++ app/src/main/java/nextstep/payments/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package nextstep.payments.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import nextstep.payments.R
+
+val RobotoRegular = FontFamily(Font(resId = R.font.roboto_regular, weight = FontWeight.W400))
+val RobotoMedium = FontFamily(Font(resId = R.font.roboto_medium, weight = FontWeight.W500))
+val RobotoBold = FontFamily(Font(resId = R.font.roboto_bold, weight = FontWeight.W700))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,8 +20,7 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    )/* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
