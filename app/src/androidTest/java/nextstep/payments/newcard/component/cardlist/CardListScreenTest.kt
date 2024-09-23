2d3e193292305d43cd5016e33768b1da28ae6b56
s9hn <tnghqn@naver.com>
2024-09-23 16:51:58 +0900

    refactor: 테스트코드 ImmutableList 적용 리팩터링

--- app/src/androidTest/java/nextstep/payments/newcard/component/cardlist/CardListScreenTest.kt
+++ app/src/androidTest/java/nextstep/payments/newcard/component/cardlist/CardListScreenTest.kt
@@ -6,6 +6,9 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import nextstep.payments.data.CardState
 import nextstep.payments.data.CardState.EmptyCard
 import nextstep.payments.ui.cardlist.CardListScreen
@@ -20,10 +23,13 @@ class CardListScreenTest {
     @Test
     fun `등록된_카드가_없을_때_카드등록요청_문구가_노출된다`() {
         // given:
+        val registeredCards: ImmutableList<CardState> = persistentListOf(EmptyCard)
+
         // when:
         composeRule.setContent {
             CardListScreen(
-                cards = listOf(EmptyCard),
+                isEmptyOfRegisteredCards = registeredCards.first() is EmptyCard,
+                cards = registeredCards,
                 onAddCardClick = { },
             )
         }
@@ -35,10 +41,13 @@ class CardListScreenTest {
     @Test
     fun `등록된_카드가_없을_때_빈_카드가_노출된다`() {
         // given:
+        val registeredCards: ImmutableList<CardState> = persistentListOf(EmptyCard)
+
         // when:
         composeRule.setContent {
             CardListScreen(
-                cards = listOf(EmptyCard),
+                isEmptyOfRegisteredCards = registeredCards.first() is EmptyCard,
+                cards = registeredCards,
                 onAddCardClick = { },
             )
         }
@@ -50,19 +59,22 @@ class CardListScreenTest {
     @Test
     fun `등록된_카드가_1개일때_등록된_카드_1개가_노출된다`() {
         // given:
+        val registeredCards: ImmutableList<CardState> = persistentListOf(
+            CardState.Card(
+                cardId = 0,
+                cardNumber = "1234123412341234",
+                expiredDate = "1221",
+                ownerName = "세훈",
+                password = "1234",
+            ),
+            EmptyCard,
+        )
+
         // when:
         composeRule.setContent {
             CardListScreen(
-                cards = listOf(
-                    CardState.Card(
-                        cardId = 0,
-                        cardNumber = "1234123412341234",
-                        expiredDate = "1221",
-                        ownerName = "세훈",
-                        password = "1234",
-                    ),
-                    EmptyCard,
-                ),
+                isEmptyOfRegisteredCards = registeredCards.first() is EmptyCard,
+                cards = registeredCards,
                 onAddCardClick = { },
             )
         }
@@ -75,18 +87,22 @@ class CardListScreenTest {
     @Test
     fun `등록된_카드가_4개_미만일때_빈_카드가_최하단에_위치한다`() {
         // given:
+        val registeredCards: ImmutableList<CardState> = (List(3) {
+            CardState.Card(
+                cardId = it.toLong(),
+                cardNumber = "1234123412341234",
+                expiredDate = "122$it",
+                ownerName = "세훈$it",
+                password = "1234",
+            )
+        } + EmptyCard).toImmutableList()
+
+
         // when:
         composeRule.setContent {
             CardListScreen(
-                cards = List(3) {
-                    CardState.Card(
-                        cardId = it.toLong(),
-                        cardNumber = "1234123412341234",
-                        expiredDate = "122$it",
-                        ownerName = "세훈$it",
-                        password = "1234",
-                    )
-                } + EmptyCard,
+                isEmptyOfRegisteredCards = registeredCards.first() is EmptyCard,
+                cards = registeredCards,
                 onAddCardClick = { },
             )
         }
@@ -103,18 +119,21 @@ class CardListScreenTest {
     @Test
     fun `등록된_카드가_4개_이상일_때_빈_카드가_사라진다`() {
         // given:
+        val registeredCards: ImmutableList<CardState> = List(4) {
+            CardState.Card(
+                cardId = it.toLong(),
+                cardNumber = "1234123412341234",
+                expiredDate = "122$it",
+                ownerName = "세훈$it",
+                password = "1234",
+            )
+        }.toImmutableList()
+
         // when:
         composeRule.setContent {
             CardListScreen(
-                cards = List(4) {
-                    CardState.Card(
-                        cardId = it.toLong(),
-                        cardNumber = "1234123412341234",
-                        expiredDate = "122$it",
-                        ownerName = "세훈$it",
-                        password = "1234",
-                    )
-                },
+                isEmptyOfRegisteredCards = registeredCards.first() is EmptyCard,
+                cards = registeredCards,
                 onAddCardClick = { },
             )
         }
