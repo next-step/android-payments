698b202f27563e02064cfb28b611d854d560c72e
s9hn <tnghqn@naver.com>
2024-09-23 16:06:47 +0900

    refactor: 버튼 리스너 적용

--- app/src/main/java/nextstep/payments/ui/cardlist/component/CardListEmptyCard.kt
+++ app/src/main/java/nextstep/payments/ui/cardlist/component/CardListEmptyCard.kt
@@ -1,8 +1,10 @@
 package nextstep.payments.ui.cardlist.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.Add
@@ -17,14 +19,19 @@ import androidx.compose.ui.unit.dp
 import nextstep.payments.R.string.cardlist_empty_card_icon
 
 @Composable
-fun CardListEmptyCard(modifier: Modifier = Modifier) {
+fun CardListEmptyCard(
+    onAddCardClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
+            .size(width = 208.dp, height = 124.dp)
             .background(
                 shape = RoundedCornerShape(size = 5.dp),
                 color = Color.Gray,
-            ),
+            )
+            .clickable { onAddCardClick() },
     ) {
         Icon(
             imageVector = Filled.Add,
@@ -40,5 +47,7 @@ fun CardListEmptyCard(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun CardListEmptyCardPreview() {
-    CardListEmptyCard()
+    CardListEmptyCard(
+        onAddCardClick = { },
+    )
 }
