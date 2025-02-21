cb2acf05aa29c8ca2daa8376785621b147bc63fe
seungohjin <corrobolea@gmail.com>
2025-02-21 20:27:43 +0900

    feat: 카드 목록 화면 UI 구현

---
+++ app/src/main/java/nextstep/payments/ui/screen/CartListScreen.kt
@@ -0,0 +1,79 @@
+package nextstep.payments.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CardListScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "새로운 카드를 등록해주세요",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.W700,
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        AddCardContainer(
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+fun AddCardContainer(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .size(width = 208.dp, height = 124.dp)
+            .background(
+                color = Color(0xFFE5E5E5),
+                shape = RoundedCornerShape(5.dp),
+            )
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = "Cart",
+            tint = Color.Black
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddCardContainerPreview() {
+    AddCardContainer(onClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardListScreenPreview() {
+    CardListScreen()
+}
