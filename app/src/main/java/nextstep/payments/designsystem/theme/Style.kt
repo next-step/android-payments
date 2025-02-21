f17008f82c9f5eaa3f5ec35b08816e6e770a40bc
parkduksung <duksung1234@naver.com>
2025-02-21 23:48:18 +0900

    style - 색상, TextStyle 추가.

---
+++ app/src/main/java/nextstep/payments/designsystem/theme/Style.kt
@@ -0,0 +1,13 @@
+package nextstep.payments.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+
+val PaymentCardTextStyle = TextStyle(
+    fontSize = 12.sp,
+    fontWeight = FontWeight.Medium,
+    color = Color.White
+)
