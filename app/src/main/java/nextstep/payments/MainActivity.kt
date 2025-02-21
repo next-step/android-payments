74b355771ce1f74f5c3af2e08b4eb705438c4ce8
parkduksung <duksung1234@naver.com>
2025-02-22 00:02:57 +0900

    refactor - MainActivity 수정.

--- app/src/main/java/nextstep/payments/MainActivity.kt
+++ app/src/main/java/nextstep/payments/MainActivity.kt
@@ -1,18 +1,35 @@
 package nextstep.payments
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import nextstep.payments.ui.newcard.NewCardScreen
-import nextstep.payments.designsystem.theme.PaymentsTheme
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import nextstep.payments.base.BaseComponentActivity
+import nextstep.payments.ui.cardlist.CardListScreen
+import nextstep.payments.ui.cardlist.CardListViewModel
+import nextstep.payments.ui.newcard.NewCardActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            PaymentsTheme {
-                NewCardScreen()
+class MainActivity : BaseComponentActivity() {
+    private val viewModel: CardListViewModel by viewModels()
+
+    @Composable
+    override fun SetContent() {
+        val launcher =
+            rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    viewModel.fetchCards()
+                }
             }
-        }
+        val uiState by viewModel.cardListUiState.collectAsStateWithLifecycle()
+        CardListScreen(
+            uiState = uiState,
+            onRouteToNewCard = {
+                launcher.launch(
+                    NewCardActivity.newInstance(context = this)
+                )
+            }
+        )
     }
 }
