276d665ac7577ce334b3c64183adab257a5b60e1
seungohjin <corrobolea@gmail.com>
2025-02-22 13:31:51 +0900

    feat: CardListViewModel getCards() > 등록 카드 갯수에 따른 UiState 분기

--- app/src/main/java/nextstep/payments/ui/viewmodel/CardListViewModel.kt
+++ app/src/main/java/nextstep/payments/ui/viewmodel/CardListViewModel.kt
@@ -2,17 +2,23 @@ package nextstep.payments.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import nextstep.payments.data.model.Card
 import nextstep.payments.data.repository.PaymentCardsRepository
+import nextstep.payments.ui.CardUiState
 
 class CardListViewModel(
     private val paymentRepsoitory: PaymentCardsRepository = PaymentCardsRepository,
 ): ViewModel() {
-    private var _cards = MutableStateFlow<List<Card>>(emptyList())
-    val cards: StateFlow<List<Card>> get() = _cards
+    private var _cards = MutableStateFlow<CardUiState<List<Card>>>(CardUiState.Empty)
+    val cards = _cards.asStateFlow()
 
     fun getCards() {
-        _cards.value = paymentRepsoitory.cards
+        val result = paymentRepsoitory.cards
+        _cards.value = when {
+            result.isEmpty() -> CardUiState.Empty
+            result.size == 1 -> CardUiState.One(result.first()) // ✅ UiState<Card>
+            else -> CardUiState.Many(result) // ✅ UiState<List<Card>>
+        }
     }
 }
