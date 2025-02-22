55080d847b87d9bb81e409a73aef7dce9e00547e
seungohjin <corrobolea@gmail.com>
2025-02-22 15:35:17 +0900

    feat: NewCardScreen 각 입력필드 컴포넌트 분리 및 입력 로직 개편

--- app/src/main/java/nextstep/payments/ui/screen/NewCardScreen.kt
+++ app/src/main/java/nextstep/payments/ui/screen/NewCardScreen.kt
@@ -6,20 +6,22 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import nextstep.payments.ui.screen.component.NewCardTopBar
+import nextstep.payments.ui.screen.component.OutlinedInputField
 import nextstep.payments.ui.screen.component.PaymentCard
+import nextstep.payments.ui.utils.CardNumberVisualTransformation
+import nextstep.payments.ui.utils.ExpiryDateVisualTransformation
 import nextstep.payments.ui.viewmodel.NewCardViewModel
 
 @Composable
@@ -44,7 +46,7 @@ fun NewCardScreen(
         setOwnerName = viewModel::setOwnerName,
         setPassword = viewModel::setPassword,
         onBackCLick = navigateToCardList,
-        onSaveClick =  {
+        onSaveClick = {
             viewModel.addCard(cardNumber, expiredDate, ownerName, password)
             navigateToCardList()
         }
@@ -78,44 +80,148 @@ fun NewCardScreen(
         ) {
             Spacer(modifier = Modifier.height(14.dp))
 
-            PaymentCard()
+            PaymentCard(
+                cardNumber = cardNumber,
+                expiredDate = expiredDate,
+                ownerName = ownerName,
+            )
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            OutlinedTextField(
-                value = cardNumber,
-                onValueChange = setCardNumber,
-                label = { Text("카드 번호") },
-                placeholder = { Text("0000 - 0000 - 0000 - 0000") },
+            CardNumberInputField(
+                cardNumber = cardNumber,
                 modifier = Modifier.fillMaxWidth(),
+                onValueChange = { cardNum ->
+                    if (cardNum.length <= 16) setCardNumber(cardNum)
+                },
             )
 
-            OutlinedTextField(
-                value = expiredDate,
-                onValueChange = setExpiredDate,
-                label = { Text("만료일") },
-                placeholder = { Text("MM / YY") },
+            ExpireDateInputField(
+                expiredDate = expiredDate,
                 modifier = Modifier.fillMaxWidth(),
+                onValueChange = { expiredDate ->
+                    if (expiredDate.length <= 4) {
+                        setExpiredDate(expiredDate)
+                    }
+                },
             )
 
-            OutlinedTextField(
-                value = ownerName,
+            OwnerNameInputField(
+                ownerName = ownerName,
+                modifier = Modifier.fillMaxWidth(),
                 onValueChange = setOwnerName,
-                label = { Text("카드 소유자 이름(선택)") },
-                placeholder = { Text("카드에 표시된 이름을 입력하세요.") },
-                modifier = Modifier.fillMaxWidth(),
             )
 
-            OutlinedTextField(
-                value = password,
-                onValueChange = setPassword,
-                label = { Text("비밀번호") },
-                placeholder = { Text("0000") },
+            PasswordInputField(
+                password = password,
                 modifier = Modifier.fillMaxWidth(),
-                visualTransformation = PasswordVisualTransformation(),
+                onValueChange = setPassword,
             )
+
         }
     }
+}
+
+@Composable
+private fun CardNumberInputField(
+    cardNumber: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedInputField(
+        value = cardNumber,
+        label = "카드 번호",
+        placeholder = "0000 - 0000 - 0000 - 0000",
+        modifier = modifier,
+        keyboardType = KeyboardType.Number,
+        visualTransformation = CardNumberVisualTransformation(),
+        onValueChange = onValueChange,
+    )
+}
+
+@Composable
+private fun ExpireDateInputField(
+    expiredDate: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedInputField(
+        value = expiredDate,
+        label = "만료일",
+        placeholder = "MM / YY",
+        modifier = modifier,
+        keyboardType = KeyboardType.Number,
+        visualTransformation = ExpiryDateVisualTransformation(),
+        onValueChange = onValueChange,
+    )
+}
+
+@Composable
+private fun OwnerNameInputField(
+    ownerName: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedInputField(
+        value = ownerName,
+        label = "카드 소유자 이름(선택)",
+        placeholder = "카드에 표시된 이름을 입력하세요.",
+        keyboardType = KeyboardType.Text,
+        modifier = modifier,
+        onValueChange = onValueChange,
+    )
+}
+
+@Composable
+private fun PasswordInputField(
+    password: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedInputField(
+        value = password,
+        label = "비밀번호",
+        placeholder = "0000",
+        modifier = modifier,
+        visualTransformation = PasswordVisualTransformation(),
+        onValueChange = onValueChange,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CardNumberInputFieldPreview() {
+    CardNumberInputField(
+        cardNumber = "1234 - 5678 - 1234 - 5678",
+        onValueChange = {},
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ExpireDateInputFieldPreview() {
+    ExpireDateInputField(
+        expiredDate = "12 / 34",
+        onValueChange = {},
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OwnerNameInputFieldPreview() {
+    OwnerNameInputField(
+        ownerName = "홍길동",
+        onValueChange = {},
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PasswordInputFieldPreview() {
+    PasswordInputField(
+        password = "1234",
+        onValueChange = {},
+    )
 }
 
 @Preview(showBackground = true)
