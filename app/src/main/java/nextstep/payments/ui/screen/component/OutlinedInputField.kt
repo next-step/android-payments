d1ade42af06e804a4823ff21cebd63771cde1c43
seungohjin <corrobolea@gmail.com>
2025-02-22 15:34:53 +0900

    feat: OutlinedInputField 공통 컴포넌트 추가

---
+++ app/src/main/java/nextstep/payments/ui/screen/component/OutlinedInputField.kt
@@ -0,0 +1,47 @@
+package nextstep.payments.ui.screen.component
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OutlinedInputField(
+    value: String,
+    label: String,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    keyboardType: KeyboardType = KeyboardType.Unspecified,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        label = { Text(label) },
+        placeholder = { Text(placeholder) },
+        modifier = modifier,
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = ImeAction.Done
+        ),
+        visualTransformation = visualTransformation,
+        onValueChange = onValueChange,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OutlinedInputFieldPreview() {
+    OutlinedInputField(
+        value = "",
+        label = "Card Number",
+        placeholder = "1234 1234 1234 1234",
+        onValueChange = { }
+    )
+}
