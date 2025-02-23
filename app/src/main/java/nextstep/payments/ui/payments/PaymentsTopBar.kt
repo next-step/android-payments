6858e1033279a4d95733ee1427da8c589904bdbd
ver <ver@sendy.ai>
2025-02-23 17:26:18 +0900

    feat: 카드 목록 상단 바를 구현한다

---
+++ app/src/main/java/nextstep/payments/ui/payments/PaymentsTopBar.kt
@@ -0,0 +1,49 @@
+package nextstep.payments.ui.payments
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight.Companion.W700
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.sp
+import nextstep.payments.ui.theme.PaymentsTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PaymentsTopBar(
+    onAddClick: () -> Unit,
+    isAddable: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = "Payments", fontSize = 22.sp) },
+        actions = {
+            if (isAddable) {
+                TextButton(onClick = { onAddClick() }) {
+                    Text(text = "추가", fontWeight = W700, fontSize = 18.sp, color = Color.Black)
+                }
+            }
+        },
+        modifier = modifier
+    )
+}
+
+class PaymentsTopBarPreviewParameterProvider : PreviewParameterProvider<Boolean> {
+    override val values = sequenceOf(true, false)
+}
+
+@Preview
+@Composable
+private fun PaymentsTopBarPreview(
+    @PreviewParameter(PaymentsTopBarPreviewParameterProvider::class) isAddable: Boolean
+) {
+    PaymentsTheme {
+        PaymentsTopBar(onAddClick = {}, isAddable = isAddable)
+    }
+}
