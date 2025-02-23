c30f983df2dfab743614a7027b2a920cf24bde8c
ver <ver@sendy.ai>
2025-02-23 16:02:11 +0900

    feat: 카드 저장소를 정의한다

---
+++ app/src/main/java/nextstep/payments/repository/PaymentCardsRepository.kt
@@ -0,0 +1,13 @@
+package nextstep.payments.repository
+
+import nextstep.payments.model.Card
+
+object PaymentCardsRepository {
+
+    private val _cards = mutableListOf<Card>()
+    val cards: List<Card> get() = _cards.toList()
+
+    fun addCard(card: Card) {
+        _cards.add(card)
+    }
+}
