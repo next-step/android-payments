dbb37ae1369056c70eebc0babdea5b74d22c22df
parkduksung <duksung1234@naver.com>
2025-02-23 22:23:57 +0900

    feat - NewCardViewModel 기능 추가.

--- app/src/main/java/nextstep/payments/ui/newcard/NewCardViewModel.kt
+++ app/src/main/java/nextstep/payments/ui/newcard/NewCardViewModel.kt
@@ -4,12 +4,19 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import nextstep.payments.constants.DateConstants.YEAR_DATE_FORMAT
 import nextstep.payments.data.PaymentCardsRepository
 import nextstep.payments.model.Card
+import nextstep.payments.model.BankType
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 class NewCardViewModel(
     private val repository: PaymentCardsRepository = PaymentCardsRepository
 ) : ViewModel() {
+
+    private val _selectBank = MutableStateFlow(BankType.NOT_SELECTED)
+    val selectBank: StateFlow<BankType> = _selectBank.asStateFlow()
 
     private val _cardAdded = MutableStateFlow(false)
     val cardAdded: StateFlow<Boolean> = _cardAdded.asStateFlow()
@@ -42,11 +49,21 @@ class NewCardViewModel(
         _password.value = password.take(4)
     }
 
+    fun setBankType(bankType: BankType) {
+        _selectBank.value = bankType
+    }
+
     fun addCard() {
+        val toYearMonth = YearMonth.parse(
+            expiredDate.value,
+            DateTimeFormatter.ofPattern(YEAR_DATE_FORMAT)
+        )
+
         repository.addCard(
             Card(
+                type = selectBank.value,
                 number = cardNumber.value,
-                expiredDate = expiredDate.value,
+                expiredDate = toYearMonth,
                 ownerName = ownerName.value,
                 password = password.value
             )
