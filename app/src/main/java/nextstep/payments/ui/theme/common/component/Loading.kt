2b27bafa23877bc266a8e5a277f5bdc6a775f30e
KimMooHyeon <dlfb77@gmail.com>
2025-02-24 02:09:44 +0900

    refactor : Loading 컴포넌트 testTag 추가

--- app/src/main/java/nextstep/payments/ui/theme/common/component/Loading.kt
+++ app/src/main/java/nextstep/payments/ui/theme/common/component/Loading.kt
@@ -6,13 +6,14 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.payments.ui.theme.PaymentsTheme
 
 @Composable
 fun Loading() {
     Box(modifier = Modifier.fillMaxSize()) {
-        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center).testTag("Loading"))
     }
 }
 
