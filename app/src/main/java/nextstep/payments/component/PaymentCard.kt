397a2be8767e56a6ee44be471ee41fcbb2afe99f
yangsooplus <yangsooplus@naver.com>
2025-02-24 22:52:08 +0900

    feat: 빈 카드 컴포넌트 작성

--- app/src/main/java/nextstep/payments/component/PaymentCard.kt
+++ app/src/main/java/nextstep/payments/component/PaymentCard.kt
@@ -2,16 +2,24 @@ package nextstep.payments.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nextstep.payments.R
+import nextstep.payments.ui.theme.PaymentsTheme
 
 @Composable
 fun PaymentCard(
@@ -36,5 +44,39 @@ fun PaymentCard(
                     shape = RoundedCornerShape(4.dp),
                 )
         )
+    }
+}
+
+@Composable
+fun EmptyPaymentCard(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = modifier
+            .shadow(8.dp)
+            .size(width = 208.dp, height = 124.dp)
+            .background(
+                color = Color(0xFFE5E5E5),
+                shape = RoundedCornerShape(5.dp),
+            )
+    ) {
+        Icon(
+            modifier = Modifier.size(20.dp).align(Alignment.Center),
+            imageVector = Icons.Filled.Add,
+            contentDescription = stringResource(R.string.add),
+            tint = Color(0xFF575757)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PaymentCardPreview() {
+    PaymentsTheme {
+        Column {
+            PaymentCard()
+            EmptyPaymentCard()
+        }
     }
 }
