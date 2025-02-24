415eb00ac483998b2c2a7361aaa95e8f0cf32d7a
parkduksung <duksung1234@naver.com>
2025-02-24 20:17:51 +0900

    refactor - PaymentCardsRepository 수정.

--- app/src/main/java/nextstep/payments/data/PaymentCardsRepository.kt
+++ app/src/main/java/nextstep/payments/data/PaymentCardsRepository.kt
@@ -4,12 +4,11 @@ import nextstep.payments.model.Card
 
 object PaymentCardsRepository {
 
-    private val _cards = mutableListOf<Card>()
-    val cards: List<Card> get() = _cards.toList()
+    private val _cards = LinkedHashMap<Int, Card>()
+    val cards: List<Card> get() = _cards.values.toList()
 
-    fun addCard(card: Card) {
-        _cards.add(card)
-    }
+    fun upsertCard(card: Card): Boolean =
+        _cards.put("${card.type.bankName}${card.number}".hashCode(), card) != card
 
     fun clear() {
         _cards.clear()
