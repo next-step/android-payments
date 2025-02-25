780d3a0d3b65333c1c5992d01035d1c49ce4a830
parkduksung <duksung1234@naver.com>
2025-02-25 13:05:05 +0900

    refactor - NewCardTopBar 수정

--- app/src/main/java/nextstep/payments/ui/newcard/component/NewCardTopBar.kt
+++ app/src/main/java/nextstep/payments/ui/newcard/component/NewCardTopBar.kt
@@ -1,18 +1,13 @@
 package nextstep.payments.ui.newcard.component
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.payments.R
+import nextstep.payments.designsystem.component.CardTopBar
 import nextstep.payments.designsystem.theme.PaymentsTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,24 +17,10 @@ fun NewCardTopBar(
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    TopAppBar(
+    CardTopBar(
         title = { Text(stringResource(R.string.top_app_bar_new_card_title)) },
-        navigationIcon = {
-            IconButton(onClick = { onBackClick() }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "뒤로 가기",
-                )
-            }
-        },
-        actions = {
-            IconButton(onClick = { onSaveClick() }) {
-                Icon(
-                    imageVector = Icons.Filled.Check,
-                    contentDescription = "완료",
-                )
-            }
-        },
+        onBackClick = onBackClick,
+        onSaveClick = onSaveClick,
         modifier = modifier
     )
 }
