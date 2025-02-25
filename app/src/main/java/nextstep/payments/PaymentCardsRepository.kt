527490bf8ce378bc7b16d13686a5015523cd01ba
hyemdooly <hyemdooly@gmail.com>
2025-02-26 00:39:00 +0900

    feat: 카드 레포지토리 추가

---
+++ app/src/main/java/nextstep/payments/PaymentCardsRepository.kt
@@ -0,0 +1,12 @@
+package nextstep.payments
+
+import nextstep.payments.model.Card
+
+object PaymentCardsRepository {
+    private val _cards = mutableListOf<Card>()
+    val cards: List<Card> get() = _cards.toList()
+
+    fun addCard(card: Card) {
+        _cards.add(card)
+    }
+}
