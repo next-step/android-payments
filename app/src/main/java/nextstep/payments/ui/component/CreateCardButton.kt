678fed04829221abfd15ba09e38ae72696f8126a
fbghgus123 <fbghgus123@naver.com>
2025-02-26 20:40:20 +0900

    feat(cardlist): 카드 추가 버튼 UI 구현

---
+++ app/src/main/java/nextstep/payments/ui/component/CreateCardButton.kt
@@ -0,0 +1,47 @@
+package nextstep.payments.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.payments.ui.theme.PaymentsTheme
+
+@Composable
+fun CreateCardButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .size(width = 208.dp, height = 124.dp)
+            .background(
+                color = Color(0xFFE5E5E5),
+                shape = RoundedCornerShape(5.dp),
+            )
+            .clickable { onClick.invoke() }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = Icons.Filled.Add.name
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun NewCardButtonPreview() {
+    PaymentsTheme {
+        CreateCardButton({})
+    }
+}
