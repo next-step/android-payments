42e774ccd141d8d5cff6a7ab03efcf77430a93fc
Bandal <no1msh1217@gmail.com>
2025-02-27 12:19:03 +0900

    feat: 카드 목록 뷰모델에서 카드 목록 UiState를 관리

---
+++ app/src/main/java/nextstep/payments/screens/card/list/CardListViewModel.kt
@@ -0,0 +1,11 @@
+package nextstep.payments.screens.card.list
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class CardListViewModel: ViewModel() {
+    private val _uiState: MutableStateFlow<CardListUiState> = MutableStateFlow(CardListUiState.Empty)
+    private val uiState: StateFlow<CardListUiState> = _uiState.asStateFlow()
+}
