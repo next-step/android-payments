4a8496aaf38a7f1ee2cb23e00d378884022febd0
fbghgus123 <fbghgus123@naver.com>
2025-02-27 09:53:22 +0900

    test(cardlist): 카드 목록 화면 상단 바 추가 버튼 노출 여부 검증 테스트 코드 작성

--- app/src/androidTest/java/nextstep/payments/CardListScreenTest.kt
+++ app/src/androidTest/java/nextstep/payments/CardListScreenTest.kt
@@ -1,6 +1,7 @@
 package nextstep.payments
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import nextstep.payments.base.BaseComposableTest
@@ -55,5 +56,114 @@ class CardListScreenTest: BaseComposableTest() {
         composeTestRule
             .onNodeWithTag("CreateCardButton")
             .assertIsDisplayed()
+    }
+    
+    @Test
+    fun `카드_목록에_카드가_여러_개_있을_때_카드_추가_UI가_노출되지_않는다`() {
+        // given
+        val cards = listOf(
+            Card(
+                id = 1,
+                cardNumber = "1234-5678-1234-5678",
+                expiredDate = "12/34",
+                ownerName = "홍길동",
+                password = "1234",
+            ),
+            Card(
+                id = 2,
+                cardNumber = "1234-5678-1234-5678",
+                expiredDate = "12/34",
+                ownerName = "홍길동",
+                password = "1234",
+            ),
+            Card(
+                id = 3,
+                cardNumber = "1234-5678-1234-5678",
+                expiredDate = "12/34",
+                ownerName = "홍길동",
+                password = "1234",
+            )
+        )
+
+        // when
+        composeTestRule.setContent {
+            CardListScreen(
+                cards = cards,
+                cardCount = CardCount.CARDS,
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithTag("CreateCardButton")
+            .assertIsNotDisplayed()
+    }
+
+    @Test
+    fun `카드_목록에_카드가_여러_개_있을_때_상단_바에_카드_추가_버튼이_노출된다`() {
+        // given
+        val cards = listOf(
+            Card(
+                id = 1,
+                cardNumber = "1234-5678-1234-5678",
+                expiredDate = "12/34",
+                ownerName = "홍길동",
+                password = "1234",
+            ),
+            Card(
+                id = 2,
+                cardNumber = "1234-5678-1234-5678",
+                expiredDate = "12/34",
+                ownerName = "홍길동",
+                password = "1234",
+            ),
+            Card(
+                id = 3,
+                cardNumber = "1234-5678-1234-5678",
+                expiredDate = "12/34",
+                ownerName = "홍길동",
+                password = "1234",
+            )
+        )
+
+        // when
+        composeTestRule.setContent {
+            CardListScreen(
+                cards = cards,
+                cardCount = CardCount.CARDS,
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("추가")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun `카드_목록에_카드가_한_개_있을_때_상단_바에_카드_추가_버튼이_노출되지_않는다`() {
+        // given
+        val cards = listOf(
+            Card(
+                id = 1,
+                cardNumber = "1234-5678-1234-5678",
+                expiredDate = "12/34",
+                ownerName = "홍길동",
+                password = "1234",
+            )
+        )
+
+        // when
+        composeTestRule.setContent {
+            CardListScreen(
+                cards = cards,
+                cardCount = CardCount.ONE_CARD,
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("추가")
+            .assertIsNotDisplayed()
     }
 }
