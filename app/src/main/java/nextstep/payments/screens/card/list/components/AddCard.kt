83088592838bda88627c4573ef5fe383884cf37f
Bandal <no1msh1217@gmail.com>
2025-02-28 17:32:10 +0900

    refactor: 컨테이너에 클릭 영역이 필요한 경우 Surface 사용

--- app/src/main/java/nextstep/payments/screens/card/list/components/AddCard.kt
+++ app/src/main/java/nextstep/payments/screens/card/list/components/AddCard.kt
@@ -1,17 +1,15 @@
 package nextstep.payments.screens.card.list.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,18 +22,18 @@ fun AddCard(
     onAddCardClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier
-            .size(width = 208.dp, height = 124.dp)
-            .background(color = Color(0xFFE5E5E5), shape = RoundedCornerShape(5.dp))
-            .clip(shape = RoundedCornerShape(5.dp))
-            .clickable(onClick = onAddCardClick),
-        contentAlignment = Alignment.Center,
+    Surface(
+        shape = RoundedCornerShape(5.dp),
+        color = Color(0xFFE5E5E5),
+        modifier = modifier.size(width = 208.dp, height = 124.dp),
+        onClick = onAddCardClick,
     ) {
-        Icon(
-            imageVector = Icons.Filled.Add,
-            contentDescription = stringResource(R.string.card_list_add_card_description),
-        )
+        Box(contentAlignment = Alignment.Center) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = stringResource(R.string.card_list_add_card_description),
+            )
+        }
     }
 }
 
