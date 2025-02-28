2f67dba18aebf937fda8b2c66407ae4e64508e29
Bandal <no1msh1217@gmail.com>
2025-02-28 18:03:00 +0900

    refactor: 빈 카드를 Base 카드를 사용하여 구현

--- app/src/main/java/nextstep/payments/components/card/EmptyPaymentCard.kt
+++ app/src/main/java/nextstep/payments/components/card/EmptyPaymentCard.kt
@@ -1,37 +1,26 @@
 package nextstep.payments.components.card
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nextstep.payments.components.card.elements.IcChip
-import nextstep.payments.ui.theme.Black100
 import nextstep.payments.ui.theme.PaymentsTheme
 
 @Composable
 fun EmptyPaymentCard(
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        contentAlignment = Alignment.CenterStart,
-        modifier = modifier
-            .shadow(8.dp)
-            .size(width = 208.dp, height = 124.dp)
-            .background(
-                color = Black100,
-                shape = RoundedCornerShape(5.dp),
-            )
-    ) {
-        IcChip(
-            modifier = Modifier.padding(start = 14.dp, bottom = 10.dp)
-        )
+    BaseCard(modifier = modifier) {
+        Spacer(Modifier.weight(1f))
+        IcChip()
+        Spacer(Modifier.height(10.dp))
+        Spacer(Modifier.weight(1f))
     }
 }
 
