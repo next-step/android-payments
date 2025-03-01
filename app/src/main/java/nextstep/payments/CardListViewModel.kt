e8601f3db99b97afdca4a750e4ff2ffcd20444c5
hyemdooly <hyemdooly@gmail.com>
2025-03-01 19:34:24 +0900

    feat: CardListViewModel 데이터 fetch 로직 추가

--- app/src/main/java/nextstep/payments/CardListViewModel.kt
+++ app/src/main/java/nextstep/payments/CardListViewModel.kt
@@ -1,12 +1,23 @@
 package nextstep.payments
 
 import androidx.lifecycle.ViewModel
-import nextstep.payments.model.Card
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import nextstep.payments.model.CardUiState
 
 class CardListViewModel(
     private val cardsRepository: PaymentCardsRepository = PaymentCardsRepository
 ) : ViewModel() {
-    private val _cards = mutableListOf<Card>()
-    val cards: List<Card>
-        get() = _cards.toList()
+    private val _uiState = MutableStateFlow<CardUiState>(CardUiState.Empty)
+    val uiState: StateFlow<CardUiState> = _uiState.asStateFlow()
+
+    fun fetchCards() {
+        val size = cardsRepository.cards.size
+        _uiState.value = when (size) {
+            0 -> CardUiState.Empty
+            1 -> CardUiState.One(cardsRepository.cards.first())
+            else -> CardUiState.Many(cardsRepository.cards)
+        }
+    }
 }
