f993eabced62a22920a677aa77eea3f2025c6b48
yangsooplus <yangsooplus@naver.com>
2025-03-03 00:36:48 +0900

    feat: PaymentCard 컴포넌트에 카드 정보 표시

--- app/src/main/java/nextstep/payments/component/PaymentCard.kt
+++ app/src/main/java/nextstep/payments/component/PaymentCard.kt
@@ -1,24 +1,30 @@
 package nextstep.payments.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nextstep.payments.R
 import nextstep.payments.model.CreditCard
 import nextstep.payments.ui.theme.PaymentsTheme
@@ -28,6 +34,9 @@ fun PaymentCard(
     card: CreditCard,
     modifier: Modifier = Modifier,
 ) {
+    val formattedNumber = remember(key1 = card.number) { card.number.formatCardNumber() }
+    val formattedDueDate = remember(key1 = card.dueDate) { card.dueDate.formatCardDueDate() }
+
     Box(
         contentAlignment = Alignment.CenterStart,
         modifier = modifier
@@ -36,18 +45,70 @@ fun PaymentCard(
             .background(
                 color = Color(0xFF333333),
                 shape = RoundedCornerShape(5.dp),
-            ).testTag("PaymentCard")
+            )
+            .testTag("PaymentCard")
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .padding(start = 14.dp, bottom = 10.dp)
-                .size(width = 40.dp, height = 26.dp)
-                .background(
-                    color = Color(0xFFCBBA64),
-                    shape = RoundedCornerShape(4.dp),
+                .padding(horizontal = 14.dp, vertical = 10.dp)
+                .align(Alignment.BottomStart),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .size(width = 40.dp, height = 26.dp)
+                    .background(
+                        color = Color(0xFFCBBA64),
+                        shape = RoundedCornerShape(4.dp),
+                    )
+            )
+
+            PaymentCardText(
+                text = formattedNumber
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                PaymentCardText(
+                    text = card.name,
                 )
-        )
+                PaymentCardText(
+                    text = formattedDueDate
+                )
+            }
+        }
     }
+}
+
+private fun String.formatCardNumber(): String {
+    val builder = StringBuilder()
+    builder.append(this.take(8))
+    if (this.length - 8 > 0) {
+        builder.append("*".repeat(this.length - 8))
+    }
+
+    return builder.toString().chunked(4).joinToString(" - ")
+}
+
+private fun String.formatCardDueDate(): String {
+    return this.take(4).chunked(2).joinToString("/")
+}
+
+@Composable
+private fun PaymentCardText(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        color = Color.White,
+        fontSize = 12.sp,
+        lineHeight = 14.sp,
+        letterSpacing = 2.sp
+    )
 }
 
 @Composable
@@ -66,7 +127,9 @@ fun EmptyPaymentCard(
             .testTag("EmptyPaymentCard")
     ) {
         Icon(
-            modifier = Modifier.size(20.dp).align(Alignment.Center),
+            modifier = Modifier
+                .size(20.dp)
+                .align(Alignment.Center),
             imageVector = Icons.Filled.Add,
             contentDescription = stringResource(R.string.add),
             tint = Color(0xFF575757)
@@ -79,7 +142,14 @@ fun EmptyPaymentCard(
 private fun PaymentCardPreview() {
     PaymentsTheme {
         Column {
-            PaymentCard(card = CreditCard.emptyCard)
+            PaymentCard(
+                card = CreditCard(
+                    number = "1111222233334444",
+                    dueDate = "0125",
+                    password = "1234",
+                    name = "YANG"
+                )
+            )
             EmptyPaymentCard()
         }
     }
