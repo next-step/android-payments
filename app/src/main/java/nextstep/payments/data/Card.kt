c7e21a62f579616d4c8000a5de69e64a2fcbba57
YoonJam <yhw1904@gmail.com>
2025-03-03 17:28:09 +0900

    feat: 더미 데이터 추가

--- app/src/main/java/nextstep/payments/data/Card.kt
+++ app/src/main/java/nextstep/payments/data/Card.kt
@@ -6,3 +6,31 @@ data class Card(
     val ownerName: String,
     val password: String,
 )
+
+
+val dummyDataList = arrayListOf(
+    Card(
+        cardNumber = "0134 - 5678 - **** - ****",
+        expiredDate = "00 / 00",
+        ownerName = "홍길동",
+        password = "0000"
+    ),
+    Card(
+        cardNumber = "1223 - 5678 - **** - ****",
+        expiredDate = "00 / 00",
+        ownerName = "김철수",
+        password = "0000"
+    ),
+    Card(
+        cardNumber = "1234 - 5678 - **** - ****",
+        expiredDate = "00 / 00",
+        ownerName = "김민수",
+        password = "0000"
+    ),
+    Card(
+        cardNumber = "1234 - 1241 - **** - ****",
+        expiredDate = "00 / 00",
+        ownerName = "김영식",
+        password = "0000"
+    )
+)
