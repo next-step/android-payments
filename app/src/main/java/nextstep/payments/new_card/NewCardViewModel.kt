b39ae9c4f8a1524a4d55687a1bb1bd2997bad5f9
YoonJam <yhw1904@gmail.com>
2025-03-03 12:16:49 +0900

    feat: repository 추가

--- app/src/main/java/nextstep/payments/new_card/NewCardViewModel.kt
+++ app/src/main/java/nextstep/payments/new_card/NewCardViewModel.kt
@@ -4,8 +4,14 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import nextstep.payments.data.Card
+import nextstep.payments.repository.PaymentCardsRepository
 
-class NewCardViewModel : ViewModel() {
+class NewCardViewModel(
+    private val repository: PaymentCardsRepository = PaymentCardsRepository
+) : ViewModel() {
+    private val _cardAdded = MutableStateFlow<Boolean>(false)
+    val cardAdded: StateFlow<Boolean> = _cardAdded.asStateFlow()
 
     private val _cardNumber = MutableStateFlow("")
     val cardNumber: StateFlow<String> = _cardNumber.asStateFlow()
@@ -33,5 +39,10 @@ class NewCardViewModel : ViewModel() {
 
     fun setPassword(password: String) {
         _password.value = password
+    }
+
+    fun addCard(card: Card) {
+        repository.addCard(card)
+        _cardAdded.value = true
     }
 }
