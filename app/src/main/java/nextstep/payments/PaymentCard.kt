3a77e8562864d1a1e893c9acace17eb86e2556d2
YoonJam <yhw1904@gmail.com>
2025-03-03 14:24:17 +0900

    reafctor: 카드 종류별로 컴포저블 분리

--- app/src/main/java/nextstep/payments/PaymentCard.kt
+++ app/src/main/java/nextstep/payments/PaymentCard.kt
@@ -1,16 +1,27 @@
 package nextstep.payments
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.payments.data.Card
 
 @Composable
 fun PaymentCard(
@@ -36,4 +47,118 @@ fun PaymentCard(
                 )
         )
     }
+}
+
+@Composable
+fun PaymentListCard(
+    card: Card,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .shadow(8.dp)
+            .size(width = 208.dp, height = 124.dp)
+            .background(
+                color = Color(0xFF333333),
+                shape = RoundedCornerShape(5.dp),
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 14.dp)
+        ) {
+            Spacer(modifier = Modifier.size(44.dp))
+            Box(
+                modifier = Modifier
+                    .size(width = 40.dp, height = 26.dp)
+                    .background(
+                        color = Color(0xFFCBBA64),
+                        shape = RoundedCornerShape(4.dp),
+                    )
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = card.cardNumber,
+                modifier = Modifier.fillMaxWidth(),
+                color = Color.White,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.W500,
+                letterSpacing = (12.sp * 0.17)
+            )
+            Spacer(modifier = Modifier.size(2.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = card.ownerName,
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.W500,
+                    letterSpacing = (12.sp * 0.1)
+                )
+                Text(
+                    text = card.expiredDate,
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.W500,
+                    letterSpacing = (12.sp * 0.08),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun EnrollmentPaymentCard(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+) {
+    Box(
+        modifier = modifier
+            .shadow(8.dp)
+            .size(width = 208.dp, height = 124.dp)
+            .background(
+                color = Color(0xFFE5E5E5),
+                shape = RoundedCornerShape(5.dp),
+            )
+            .clickable {
+                onClick()
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "+",
+            color = Color(0xFF575757),
+            fontSize = 34.sp,
+            fontWeight = FontWeight.W400,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PaymentCardTypePreview() {
+    PaymentCard()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PaymentCardListTypePreview() {
+    val card = Card(
+        cardNumber = "1111 - 2222 - **** - ****",
+        ownerName = "홍길동",
+        expiredDate = "12/24",
+        password = "0000",
+    )
+
+    PaymentListCard(card = card)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun EnrollmentPaymentCardTypePreview() {
+    EnrollmentPaymentCard()
 }
