dc814f10aa265e8e7e051245c9434151038a967a
seungohjin <corrobolea@gmail.com>
2025-03-03 13:50:09 +0900

    refactor: 사용하지 않는 코드 제거

--- app/src/main/java/nextstep/payments/ui/screen/CardApp.kt
+++ app/src/main/java/nextstep/payments/ui/screen/CardApp.kt
@@ -5,12 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import nextstep.payments.ui.screen.navigation.CardRoute
 
@@ -21,8 +19,6 @@ fun CardApp(
     modifier: Modifier = Modifier,
 ) {
     val navController = rememberNavController()
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
 
     Scaffold(
         modifier = modifier,
