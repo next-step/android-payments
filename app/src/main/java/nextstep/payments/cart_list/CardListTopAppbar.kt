9338e3b0151e4a113cfa4670b4a9ac0d37f7c85b
YoonJam <yhw1904@gmail.com>
2025-03-03 13:36:10 +0900

    feat: 카드 리스트 top app bar 컴포저블 추가

---
+++ app/src/main/java/nextstep/payments/cart_list/CardListTopAppbar.kt
@@ -0,0 +1,60 @@
+package nextstep.payments.cart_list
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardListTopAppbar(
+    modifier: Modifier = Modifier,
+    showAddTextButton: Boolean = false,
+    onAddClick: () -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = "Payments") },
+        modifier = modifier,
+        actions = {
+            if (showAddTextButton) {
+                TextButton(
+                    onClick = onAddClick,
+                ) {
+                    Text(
+                        text = "추가",
+                        color = Color.Black,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.W700,
+                        lineHeight = 36.sp,
+                    )
+                }
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White,
+        ),
+    )
+}
+
+@Preview
+@Composable
+private fun CardListTopAppbarShowAddTextButtonPreview() {
+    CardListTopAppbar(
+        showAddTextButton = true,
+        onAddClick = {},
+    )
+}
+
+@Preview
+@Composable
+private fun CardListTopAppbarPreview() {
+    CardListTopAppbar()
+}
