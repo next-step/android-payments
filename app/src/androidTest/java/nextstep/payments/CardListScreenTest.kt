6bdc9e5ca2a804a36d45f6be0bc349bf8e07c09a
seungohjin <corrobolea@gmail.com>
2025-03-03 12:40:21 +0900

    refactor: vararg를 활용해 카드 등록 함수 개선

--- app/src/androidTest/java/nextstep/payments/CardListScreenTest.kt
+++ app/src/androidTest/java/nextstep/payments/CardListScreenTest.kt
@@ -18,55 +18,10 @@ class CardListScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    fun defaultCondition() {
+    fun registerCards(vararg cards: Card) {
         PaymentCardsRepository.clearCards()
 
-        composeTestRule.setContent {
-            CardListScreen(
-                navigateToNewCard = {},
-            )
-        }
-    }
-
-    fun registerOneCardCondition() {
-        PaymentCardsRepository.clearCards()
-
-        PaymentCardsRepository.addCard(
-            card = Card(
-                cardNumber = "1234567812345678",
-                expiredDate = "1223",
-                ownerName = "홍길동",
-                password = "1234",
-            )
-        )
-
-        composeTestRule.setContent {
-            CardListScreen(
-                navigateToNewCard = {},
-            )
-        }
-    }
-
-    fun registerTwoCardCondition() {
-        PaymentCardsRepository.clearCards()
-
-        PaymentCardsRepository.addCard(
-            card = Card(
-                cardNumber = "1234567812345678",
-                expiredDate = "1223",
-                ownerName = "홍길동",
-                password = "1234",
-            )
-        )
-
-        PaymentCardsRepository.addCard(
-            card = Card(
-                cardNumber = "1234567812345678",
-                expiredDate = "1223",
-                ownerName = "홀리물리",
-                password = "1234",
-            )
-        )
+        cards.forEach { PaymentCardsRepository.addCard(it) }
 
         composeTestRule.setContent {
             CardListScreen(
@@ -77,7 +32,7 @@ class CardListScreenTest {
 
     @Test
     fun 등록된_카드가_없을시_카드_등록_문구가_화면에_노출된다() {
-        defaultCondition()
+        registerCards()
 
         // 등록된 카드가 없을 때 카드 등록 문구가 화면에 노출되는지 확인
         composeTestRule.onNodeWithText(REGISTER_NEW_CARD_TEXT)
@@ -86,7 +41,7 @@ class CardListScreenTest {
 
     @Test
     fun 등록된_카드가_없을시_카드_등록_버튼이_화면에_노출된다() {
-        defaultCondition()
+        registerCards()
 
         // 등록된 카드가 없을 때 카드 등록 버튼이 화면에 노출되는지 확인
         composeTestRule.onNodeWithContentDescription(REGISTER_CARD_BUTTON_CONTAINER_DESCRIPTION)
@@ -95,7 +50,14 @@ class CardListScreenTest {
 
     @Test
     fun 등록된_카드가_하나_있을시_카드가_화면에_노출된다() {
-        registerOneCardCondition()
+        registerCards(
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홍길동",
+                password = "1234",
+            )
+        )
 
         // 등록된 카드가 있을 때 카드가 화면에 노출되는지 확인
         composeTestRule.onAllNodesWithText("홍길동")
@@ -107,7 +69,14 @@ class CardListScreenTest {
 
     @Test
     fun 등록된_카드가_하나일시_카드_등록_버튼이_화면에_노출된다() {
-        registerOneCardCondition()
+        registerCards(
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홍길동",
+                password = "1234",
+            )
+        )
 
         // 등록된 카드가 하나일 때 카드 등록 버튼이 화면에 노출되는지 확인
         composeTestRule.onNodeWithContentDescription(REGISTER_CARD_BUTTON_CONTAINER_DESCRIPTION)
@@ -116,7 +85,20 @@ class CardListScreenTest {
 
     @Test
     fun 등록된_카드가_2개_이상일시_카드_리스트가_화면에_노출된다() {
-        registerTwoCardCondition()
+        registerCards(
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홍길동",
+                password = "1234",
+            ),
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홀리물리",
+                password = "1234",
+            )
+        )
 
         // 등록된 카드가 있을 때 카드가 화면에 노출되는지 확인
         composeTestRule.onAllNodesWithText("홍길동")
@@ -134,7 +116,20 @@ class CardListScreenTest {
 
     @Test
     fun 등록된_카드가_2개_이상일시_앱바에_추가_버튼이_노출된다() {
-        registerTwoCardCondition()
+        registerCards(
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홍길동",
+                password = "1234",
+            ),
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홀리물리",
+                password = "1234",
+            )
+        )
 
         // 등록된 카드가 2개 이상일 때 앱바에 추가 버튼이 노출되는지 확인
         composeTestRule.onNodeWithText(ADD_CARD_APP_BAR_TEXT)
@@ -143,7 +138,20 @@ class CardListScreenTest {
 
     @Test
     fun 등록된_카드가_2개_이상일시_카드_등록_버튼이_노출되지_않는다() {
-        registerTwoCardCondition()
+        registerCards(
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홍길동",
+                password = "1234",
+            ),
+            Card(
+                cardNumber = "1234567812345678",
+                expiredDate = "1223",
+                ownerName = "홀리물리",
+                password = "1234",
+            )
+        )
 
         // 등록된 카드가 2개 이상일 때 카드 등록 버튼이 노출되지 않는지 확인
         composeTestRule.onNodeWithContentDescription(REGISTER_CARD_BUTTON_CONTAINER_DESCRIPTION)
