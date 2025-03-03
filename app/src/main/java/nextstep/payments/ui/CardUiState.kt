174010e9575b7fa2a872d4dcbac27987c3af84af
seungohjin <corrobolea@gmail.com>
2025-03-03 13:37:24 +0900

    refactor: CardUiState object Empty > data object 타입으로 수정

--- app/src/main/java/nextstep/payments/ui/CardUiState.kt
+++ app/src/main/java/nextstep/payments/ui/CardUiState.kt
@@ -3,7 +3,7 @@ package nextstep.payments.ui
 import nextstep.payments.data.model.Card
 
 sealed interface CardUiState<out T> {
-    object Empty : CardUiState<Nothing>
+    data object Empty : CardUiState<Nothing>
     data class One(val data: Card) : CardUiState<List<Card>>
     data class Many(val data: List<Card>) : CardUiState<List<Card>>
 }
