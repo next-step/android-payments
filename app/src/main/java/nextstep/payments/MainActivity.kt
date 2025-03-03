c2ffe361c10e7ee36731c15c8e6d741efdf40025
parkduksung <duksung1234@naver.com>
2025-03-03 23:08:23 +0900

    refactor - MainActivity 수정, 불필요한 부분 제거.

--- app/src/main/java/nextstep/payments/MainActivity.kt
+++ app/src/main/java/nextstep/payments/MainActivity.kt
@@ -1,41 +1,12 @@
 package nextstep.payments
 
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import nextstep.payments.base.BaseComponentActivity
-import nextstep.payments.ui.cardlist.CardListScreen
-import nextstep.payments.ui.cardlist.CardListViewModel
-import nextstep.payments.ui.newcard.NewCardActivity
-import nextstep.payments.ui.updatecard.UpdateCardActivity
+import nextstep.payments.ui.main.MainScreen
 
 class MainActivity : BaseComponentActivity() {
-    private val viewModel: CardListViewModel by viewModels()
-
     @Composable
     override fun SetContent() {
-        val launcher =
-            rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                if (it.resultCode == RESULT_OK) {
-                    viewModel.fetchCards()
-                }
-            }
-        val uiState by viewModel.cardListUiState.collectAsStateWithLifecycle()
-        CardListScreen(
-            uiState = uiState,
-            onRouteToNewCard = {
-                launcher.launch(
-                    NewCardActivity.newInstance(context = this)
-                )
-            },
-            onRouteToUpdateCard = { card ->
-                launcher.launch(
-                    UpdateCardActivity.newInstance(context = this, item = card)
-                )
-            }
-        )
+        MainScreen()
     }
 }
