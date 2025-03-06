54e53ea5bfdd262a10e17bd04c89833b44871e6c
Bandal <no1msh1217@gmail.com>
2025-03-06 21:32:05 +0900

    test: UpdateCardScreen은 UiState에 따라 다른 Screen을 표시한다.

---
+++ app/src/androidTest/java/nextstep/payments/UpdateCardScreenTest.kt
@@ -0,0 +1,87 @@
+package nextstep.payments
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import nextstep.payments.domain.Card
+import nextstep.payments.domain.CardCompany
+import nextstep.payments.screens.card.update.UpdateCardScreen
+import nextstep.payments.screens.card.update.UpdateCardUiState
+import org.junit.Rule
+import org.junit.Test
+
+class UpdateCardScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun UiState가_AddCardUiState일_때_새_카드를_등록하는_화면이_표시된다() {
+        // given
+        composeTestRule.setContent {
+            UpdateCardScreen(
+                uiState = UpdateCardUiState.AddCardUiState(),
+                onCardCompanyClick = {},
+                onCardNumberChange = {},
+                onExpiredDateChange = {},
+                onOwnerNameChange = {},
+                onPasswordChange = {},
+                onBackClick = {},
+                onSaveClick = {},
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("국민카드")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("카드 추가")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("카드 수정")
+            .assertIsNotDisplayed()
+    }
+
+    @Test
+    fun UiState가_EditCardUiState일_때_카드를_수정하는_화면이_표시된다() {
+        // given
+        composeTestRule.setContent {
+            UpdateCardScreen(
+                uiState = UpdateCardUiState.EditCardUiState(
+                    cardForEdit = Card(
+                        numbers = "1234567890123456",
+                        expiredDate = "1225",
+                        ownerName = "BANDAL",
+                        password = "1234",
+                        cardCompany = CardCompany.HANA,
+                    )
+                ),
+                onCardCompanyClick = {},
+                onCardNumberChange = {},
+                onExpiredDateChange = {},
+                onOwnerNameChange = {},
+                onPasswordChange = {},
+                onBackClick = {},
+                onSaveClick = {},
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithText("국민카드")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("카드 수정")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("카드 추가")
+            .assertIsNotDisplayed()
+    }
+}
