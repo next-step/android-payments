11fc8385188fb2196c6d5edf8b53c4df7431ad62
Bandal <no1msh1217@gmail.com>
2025-03-06 21:46:58 +0900

    test: EditCardScreen은 수정하려는 카드의 변경사항이 없다면 카드 수정을 완료할 수 없다

---
+++ app/src/androidTest/java/nextstep/payments/EditCardScreenTest.kt
@@ -0,0 +1,106 @@
+package nextstep.payments
+
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import nextstep.payments.domain.Card
+import nextstep.payments.domain.CardCompany
+import nextstep.payments.screens.card.CardCompanyState
+import nextstep.payments.screens.card.update.CardState
+import nextstep.payments.screens.card.update.EditCardScreen
+import nextstep.payments.screens.card.update.UpdateCardUiState
+import org.junit.Rule
+import org.junit.Test
+
+class EditCardScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun 수정할카드의_변경사항이_없다면_완료버튼이_비활성화다() {
+        // given
+        composeTestRule
+            .setContent {
+                val cardNumbers = "1234567890123456"
+                val expiredDate = "1225"
+                val ownerName = "BANDAL"
+                val password = "1234"
+
+                EditCardScreen(
+                    uiState = UpdateCardUiState.EditCardUiState(
+                        cardForEdit = Card(
+                            numbers = cardNumbers,
+                            expiredDate = expiredDate,
+                            ownerName = ownerName,
+                            password = password,
+                            cardCompany = CardCompany.HANA
+                        ), cardState = CardState(
+                            selectedCardCompany = CardCompanyState.HANA,
+                            cardNumber = cardNumbers,
+                            expiredDate = expiredDate,
+                            ownerName = ownerName,
+                            password = password
+                        ),
+                        cardUpdated = false
+                    ),
+                    onCardCompanyClick = {},
+                    onCardNumberChange = {},
+                    onExpiredDateChange = {},
+                    onOwnerNameChange = {},
+                    onPasswordChange = {},
+                    onBackClick = {},
+                    onSaveClick = {},
+                )
+            }
+
+        // then
+        composeTestRule
+            .onNodeWithContentDescription("완료")
+            .assertIsNotEnabled()
+    }
+
+    @Test
+    fun 수정할카드의_변경사항이_있다면_완료버튼이_비활성화다() {
+        // given
+        composeTestRule
+            .setContent {
+                val cardNumbers = "1234567890123456"
+                val expiredDate = "1225"
+                val ownerName = "BANDAL"
+                val password = "1234"
+
+                EditCardScreen(
+                    uiState = UpdateCardUiState.EditCardUiState(
+                        cardForEdit = Card(
+                            numbers = cardNumbers,
+                            expiredDate = expiredDate,
+                            ownerName = ownerName,
+                            password = password,
+                            cardCompany = CardCompany.HANA
+                        ), cardState = CardState(
+                            selectedCardCompany = CardCompanyState.KAKAO,
+                            cardNumber = cardNumbers,
+                            expiredDate = expiredDate,
+                            ownerName = ownerName,
+                            password = password
+                        ),
+                        cardUpdated = false
+                    ),
+                    onCardCompanyClick = {},
+                    onCardNumberChange = {},
+                    onExpiredDateChange = {},
+                    onOwnerNameChange = {},
+                    onPasswordChange = {},
+                    onBackClick = {},
+                    onSaveClick = {},
+                )
+            }
+
+        // then
+        composeTestRule
+            .onNodeWithContentDescription("완료")
+            .assertIsEnabled()
+    }
+}
