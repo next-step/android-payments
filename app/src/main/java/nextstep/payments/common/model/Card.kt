ec125cb9505faa6a179a0504d09dbc8221c86c86
hyemdooly <hyemdooly@gmail.com>
2025-03-16 22:37:17 +0900

    feat: Card bank 선택하지 않은 경우 추가

--- app/src/main/java/nextstep/payments/common/model/Card.kt
+++ app/src/main/java/nextstep/payments/common/model/Card.kt
@@ -1,7 +1,7 @@
 package nextstep.payments.common.model
 
 data class Card(
-    val bank: Bank,
+    val bank: Bank?,
     val cardNumber: String,
     val expiredDate: String,
     val ownerName: String,
