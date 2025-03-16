861130e19a2a3bdd4e2fc564b7d257d49278bc81
hyemdooly <hyemdooly@gmail.com>
2025-03-16 23:15:49 +0900

    fix: 카카오뱅크 색상 변경

--- app/src/main/java/nextstep/payments/common/model/Bank.kt
+++ app/src/main/java/nextstep/payments/common/model/Bank.kt
@@ -12,7 +12,7 @@ enum class Bank(
 ) {
     BC(R.string.bc_name, R.drawable.img_bc, Color(0xFFF04651)),
     SHINHAN(R.string.shinhan_name, R.drawable.img_shinhan, Color(0xFF0554F2)),
-    KAKAOBANK(R.string.kakao_name, R.drawable.img_kakaobank, Color(0xFFFFE600)),
+    KAKAOBANK(R.string.kakao_name, R.drawable.img_kakaobank, Color(0xFFE3CE10)),
     HYUNDAI(R.string.hyundai_name, R.drawable.img_hyundai, Color(0xFF000000)),
     WOORI(R.string.woori_name,R.drawable.img_woori, Color(0xFF0477BF)),
     LOTTE(R.string.lotte_name, R.drawable.img_lotte, Color(0xFFF21D2F)),
