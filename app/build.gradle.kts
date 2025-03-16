359ffc9ba8bf5bd9515e7c1108f990213aa52dc9
hyemdooly <hyemdooly@gmail.com>
2025-03-16 23:31:36 +0900

    chore: 사용하지 않는 플러그인 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.serialization)
 }
 
 android {
