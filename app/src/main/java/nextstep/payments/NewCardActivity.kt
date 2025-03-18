7140a017afbd26c3b324be473a1409a7c1536bd1
김무현 <2230198@ad.h-firework.com>
2025-03-18 21:57:08 +0900

    refactor : 상태있는 NewCardScreen 컴포져블함수 내부에서 viewModel 관련동작 및 뒤로가기 기본동작 수행하도록 변경

--- app/src/main/java/nextstep/payments/NewCardActivity.kt
+++ app/src/main/java/nextstep/payments/NewCardActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import nextstep.payments.newcard.NewCardScreen
-import nextstep.payments.newcard.NewCardViewModel
 import nextstep.payments.ui.theme.PaymentsTheme
 
 class NewCardActivity : ComponentActivity() {
-    private val viewModel: NewCardViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +15,10 @@ class NewCardActivity : ComponentActivity() {
         setContent {
             PaymentsTheme {
                 NewCardScreen(
-                    viewModel = viewModel,
                     navigateToCardList = {
                         setResult(RESULT_OK)
                         finish()
-                    },
-                    onSaveClick = viewModel::saveCard,
-                    onBackClick = { finish() }
+                    }
                 )
             }
         }
