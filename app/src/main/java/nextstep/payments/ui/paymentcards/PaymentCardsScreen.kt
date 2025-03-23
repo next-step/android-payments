fe51f1bb2daafaf31a67e9d464b49ca3233d3726
차민지 <ckalswl331@naver.com>
2025-03-23 17:52:52 +0900

    feat: 카드 목록이 비어있을 때에는 "새로운 카드를 등록해주세요" 안내가 노출

--- app/src/main/java/nextstep/payments/ui/paymentcards/PaymentCardsScreen.kt
+++ app/src/main/java/nextstep/payments/ui/paymentcards/PaymentCardsScreen.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.payments.R
 import nextstep.payments.model.CreditCard
 import nextstep.payments.ui.newcard.AddCard
 import nextstep.payments.ui.newcard.PaymentCard
@@ -34,6 +39,15 @@ fun PaymentCardsScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(cards.size + 1) { index ->
+                if (cards.isEmpty()) {
+                    Text(
+                        modifier = Modifier
+                            .padding(vertical = 32.dp),
+                        text = stringResource(R.string.add_new_card_label),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
                 if (index == cards.size) {
                     AddCard(onClick = onAddClick)
                     return@items
@@ -54,11 +68,19 @@ private fun PaymentCardsScreenPreview() {
     PaymentsTheme {
         PaymentCardsScreen(
             cards = listOf(
-                CreditCard("1111222233334444","0421","crew","1234"),
-                CreditCard("1111222233334444","0421","crew","1234"),
-                CreditCard("1111222233334444","0421","crew","1234"),
-                CreditCard("1111222233334444","0421","crew","1234"),
+                CreditCard("1111222233334444", "0421", "crew", "1234"),
+                CreditCard("1111222233334444", "0421", "crew", "1234"),
+                CreditCard("1111222233334444", "0421", "crew", "1234"),
+                CreditCard("1111222233334444", "0421", "crew", "1234"),
             )
         )
+    }
+}
+
+@Preview
+@Composable
+private fun NoCardsScreenPreview() {
+    PaymentsTheme {
+        PaymentCardsScreen(cards = listOf())
     }
 }
